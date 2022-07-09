9d1ba5546ba32ac3452cd07c3f4ed0e816f13586
Basem98 <basem.mostafa98@hotmail.com>
2022-07-09 11:47:55 +0200

    feat: Mount the parsing middleware

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const config = require('./config/envConfig');
 const { connectToDatabase } = require('./config/dbConnection');
 
-
+/* ---------- Mount the parsing middleware ---------- */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 connectToDatabase(config.DB.URL);
