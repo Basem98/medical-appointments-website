7ae6ff9925c90361f2599ace70447903dd2c96a1
Basem98 <basem.mostafa98@hotmail.com>
2022-07-09 11:54:48 +0200

    feat: Establish the initial api routing middleware

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const config = require('./config/envConfig');
 const { connectToDatabase } = require('./config/dbConnection');
+const { userRouter, doctorRouter, adminRouter } = require('./router/main.router');
+
 
 /* ---------- Mount the parsing middleware ---------- */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+
+
+
+/* ---------- Mount the routers middleware ---------- */
+app.use('/api/users', userRouter);
+app.use('/api/doctors', doctorRouter);
+app.use('/api/admin', adminRouter);
+
+
+/* ---------- Establish the server & database connections ---------- */
 connectToDatabase(config.DB.URL);
 app.listen(config.APP.PORT, () => console.log(`Server is listening on http://localhost:${config.APP.PORT}`));
---
+++ router/main.router.js
@@ -0,0 +1,9 @@
+const userRouter = require('./user.router');
+const doctorRouter = require('./doctor.router');
+const adminRouter = require('./admin.router');
+
+module.exports = {
+  userRouter,
+  doctorRouter,
+  adminRouter
+}
