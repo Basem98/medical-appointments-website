3775d7a054c8b56c263ed99fccffc62dea80745b
unknown <belalelmeligy516@gmail.com>
2022-07-09 18:24:33 +0200

    fix: Fix non-imported package bcrypt and fixed a bug when saving the hashed password in the database

--- controller/users/main.controller.js
+++ controller/users/main.controller.js
@@ -1,13 +1,16 @@
 const { validateUserData } = require('./validation.controller');
 const User = require('../../model/user.model');
+const bycrypt = require('bcrypt');
 
 module.exports.addUser = (req, res, next) => {
     let newUser = new User(req.body);
-    bcrypt.hash(req.body.password,10)
-        .then((hashedPassword) => { newUser.password = hashedPassword })
-        .catch(error => next(error))
-    newUser.save()
-        .then((data) => res.status(201).json(data))
+    bycrypt.hash(req.body.password, 10)
+        .then((hashedPassword) => {
+            newUser.password = hashedPassword;
+            newUser.save()
+                .then((data) => res.status(201).json(data))
+                .catch(error => next(error))
+        })
         .catch(error => next(error))
 }
 
