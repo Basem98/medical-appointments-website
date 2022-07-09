903b602f40a098086f043e0e6daa31a2baa69f91
unknown <belalelmeligy516@gmail.com>
2022-07-09 18:06:58 +0200

    feat: Add POST method to route /api/users to register new users to the system

--- router/user.router.js
+++ router/user.router.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
+const userController = require('../controller/users/main.controller');
+const validationMiddleware = require('../middleware/validation.middleware');
+
+
+userRouter.route('/')
+    .post(userController.validateUserData(), validationMiddleware, userController.addUser)
 
 
 module.exports = userRouter;
