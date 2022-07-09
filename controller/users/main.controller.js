15491d8b5f3ddb1f0bef622ae5edef52c3a93d20
unknown <belalelmeligy516@gmail.com>
2022-07-09 18:11:20 +0200

    feat: Implement hashing and salting user's password before saving data in the database

--- controller/users/main.controller.js
+++ controller/users/main.controller.js
@@ -3,6 +3,9 @@ const User = require('../../model/user.model');
 
 module.exports.addUser = (req, res, next) => {
     let newUser = new User(req.body);
+    bcrypt.hash(req.body.password,10)
+        .then((hashedPassword) => { newUser.password = hashedPassword })
+        .catch(error => next(error))
     newUser.save()
         .then((data) => res.status(201).json(data))
         .catch(error => next(error))
