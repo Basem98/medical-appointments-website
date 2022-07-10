12e8c5dc05814f9bfcc34237269ebf6dcff359c8
unknown <belalelmeligy516@gmail.com>
2022-07-10 18:25:56 +0200

    refactor: Add optional() to each property so that it can be reusable in PATCH requests

--- controller/users/validation.controller.js
+++ controller/users/validation.controller.js
@@ -2,10 +2,10 @@ const { body } = require('express-validator');
 
 module.exports.validateUserData = () => {
     return [
-        body('firstName').isLength({ min: 2, max: 50 }).withMessage('First Name length between 2 to 50 characters'),
-        body('lastName').isLength({ min: 2, max: 50 }).withMessage('Last Name length between 2 to 50 characters'),
-        body('email').isEmail().withMessage('Invalid Email'),
-        body('password').isStrongPassword().withMessage('Password is weak'),
-        body('phoneNumber').isMobilePhone().withMessage('Invalid Phone Number')
+        body('firstName').optional().isLength({ min: 2, max: 50 }).withMessage('First Name length between 2 to 50 characters'),
+        body('lastName').optional().isLength({ min: 2, max: 50 }).withMessage('Last Name length between 2 to 50 characters'),
+        body('email').optional().isEmail().withMessage('Invalid Email'),
+        body('password').optional().isStrongPassword().withMessage('Password is weak'),
+        body('phoneNumber').optional().isMobilePhone().withMessage('Invalid Phone Number')
     ];
 }
