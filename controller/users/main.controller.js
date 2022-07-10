102da0075fbf369f4e1c4dbaa03da4b399bb71a9
unknown <belalelmeligy516@gmail.com>
2022-07-10 15:53:03 +0200

    feat: Implement deleteUserById functionality in user controller

--- controller/users/main.controller.js
+++ controller/users/main.controller.js
@@ -46,6 +46,23 @@ module.exports.getUserById = (req, res, next) => {
         });
 }
 
+module.exports.deleteUserById = (req, res, next) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(404).json({ message: 'User is Not Found' });
+    }
+    if (req.body.id.toString() != req.params.id) {
+        return res.status(401).send({ message: 'Unauthorized to delete user data' });
+    }
+    User.deleteOne({ _id: req.body.id })
+        .then((data) => {
+            if (!data.deletedCount) {
+                return res.status(404).send({ message: 'User is Not found' });
+            }
+            return res.status(200).json({ message: 'User deleted successfully' });
+        })
+        .catch(error => next(error))
+}
+
 module.exports.loginUser = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then((data) => {
