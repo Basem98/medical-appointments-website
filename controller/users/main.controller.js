e62ca2e0d1a0b123a06f67b6181b169fbc811e97
unknown <belalelmeligy516@gmail.com>
2022-07-10 15:46:54 +0200

    refactor: Move if condition (that checks if the provided user id matches the actual user id carries in token) outside of database quety to prevent the search process in the first place

--- controller/users/main.controller.js
+++ controller/users/main.controller.js
@@ -21,17 +21,17 @@ module.exports.getUserById = (req, res, next) => {
     if (!mongoose.isValidObjectId(req.params.id)) {
         return res.status(404).json({ message: 'User is Not Found' });
     }
+    if (req.params.id.toString() != req.body.id) {
+        return res.status(401).json({ message: 'Unauthorized to show user data' });
+    }
     User.findOne({ _id: mongoose.Types.ObjectId(req.params.id) })
         .then((data) => {
             if (!data) {
                 return res.status(404).json({ message: 'User is Not Found' })
             }
-            if (req.params.id.toString() == req.body.id) {
+            else {
                 data.password = undefined; // Exclude user password from retrieved data
                 return res.status(200).json(data);
-            }
-            else {
-                return res.status(401).json({ message: 'Unauthorized to show user data' });
             }
         })
         .catch(error => next(error))
