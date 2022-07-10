9786167de8ee19cb2118a829c9674714c4593741
unknown <belalelmeligy516@gmail.com>
2022-07-10 07:27:49 +0200

    refactor: Send response with status code 400 for Incorrect email or password instead of throwing error

--- controller/users/main.controller.js
+++ controller/users/main.controller.js
@@ -41,12 +41,12 @@ module.exports.loginUser = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then((data) => {
             if (!data) {
-                throw new Error('Incorrect email or password');
+                res.status(400).json({message: 'Incorrect email or password'});
             }
             bycrypt.compare(req.body.password, data.password)
                 .then((isCorrectPassword) => {
                     if (!isCorrectPassword) {
-                        throw new Error('Incorrect email or password');
+                        res.status(400).json({message: 'Incorrect email or password'});
                     }
                     let token = jwt.sign({
                         id: data._id,
