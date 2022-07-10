47e896aac29bb2895893e660e8233462296289f8
unknown <belalelmeligy516@gmail.com>
2022-07-10 04:51:49 +0200

    feat: Implement loginUser functin in user controller to login user after checking correct email and password. Then generate jwt after success login

--- controller/users/main.controller.js
+++ controller/users/main.controller.js
@@ -1,6 +1,8 @@
 const { validateUserData } = require('./validation.controller');
 const User = require('../../model/user.model');
 const bycrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const envConfig = require('../../config/envConfig');
 
 module.exports.addUser = (req, res, next) => {
     let newUser = new User(req.body);
@@ -8,10 +10,32 @@ module.exports.addUser = (req, res, next) => {
         .then((hashedPassword) => {
             newUser.password = hashedPassword;
             newUser.save()
-                .then((data) => res.status(201).json({message: 'User added successfully'}))
+                .then((data) => res.status(201).json({ message: 'User added successfully' }))
                 .catch(error => next(error))
         })
         .catch(error => next(error))
+}
+
+module.exports.loginUser = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then((data) => {
+            if (!data) {
+                throw new Error('Incorrect email or password');
+            }
+            bycrypt.compare(req.body.password, data.password)
+                .then((isCorrectPassword) => {
+                    if (!isCorrectPassword) {
+                        throw new Error('Incorrect email or password');
+                    }
+                    let token = jwt.sign({
+                        id: data._id,
+                        role: 'user'
+                    }, envConfig.AUTH.SECRET_KEY);
+                    res.status(200).json({ token });
+                })
+                .catch((error) => next(error))
+        })
+        .catch((error) => next(error))
 }
 
 //returns validation array
