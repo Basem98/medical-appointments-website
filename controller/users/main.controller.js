e177d38258379f970619c711cbb679e8bab01eb2
unknown <belalelmeligy516@gmail.com>
2022-07-10 18:24:40 +0200

    feat: Implement updateUserById functionality in user controller

--- controller/users/main.controller.js
+++ controller/users/main.controller.js
@@ -46,6 +46,30 @@ module.exports.getUserById = (req, res, next) => {
         });
 }
 
+module.exports.updateUserById = async (req, res, next) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(404).json({ message: 'User is Not Found' });
+    }
+    if (req.params.id.toString() != req.body.id) {
+        return res.status(401).json({ message: 'Unauthorized to update user data' });
+    }
+    if (req.body.password) {
+        await bycrypt.hash(req.body.password, 10)
+            .then((hashedPassword) => {
+                req.body.password = hashedPassword;
+            })
+            .catch(error => {
+                error.statusCode = 500;
+                next(error);
+            });
+    }
+    User.findByIdAndUpdate(req.body.id, { $set: req.body })
+        .then(() => {
+            return res.status(200).json({ message: 'User updated successfully' });
+        })
+        .catch(error => next(error))
+}
+
 module.exports.deleteUserById = (req, res, next) => {
     if (!mongoose.isValidObjectId(req.params.id)) {
         return res.status(404).json({ message: 'User is Not Found' });
