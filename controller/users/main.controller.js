d9fe7b2e2611f94f14eb637ee4e108d825d29e9e
unknown <belalelmeligy516@gmail.com>
2022-07-10 18:40:01 +0200

    refactor: Add error message (Password is Missing) to catch bcrypt.hash() error  in addUser controller function

--- controller/users/main.controller.js
+++ controller/users/main.controller.js
@@ -14,6 +14,7 @@ module.exports.addUser = (req, res, next) => {
                 .then((data) => res.status(201).json({ message: 'User added successfully' }))
                 .catch(error => {
                     error.statusCode = 500;
+                    error.message = "Password is Missing"
                     next(error)
                 });
         })
