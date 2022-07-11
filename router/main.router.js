13db76ea67e2c01c05e31a2877fd21fc974b1047
Basem98 <basem.mostafa98@hotmail.com>
2022-07-11 17:42:27 +0200

    feat: Implement a /api/verify PUT endpoint to verify tokens

--- router/main.router.js
+++ router/main.router.js
@@ -1,8 +1,16 @@
+const express = require('express');
+const baseRouter = express.Router();
 const userRouter = require('./user.router');
 const doctorRouter = require('./doctor.router');
 const adminRouter = require('./admin.router');
 
+const {verifyToken} = require('../controller/base.controller');
+
+
+baseRouter.put('/verify', verifyToken);
+
 module.exports = {
+  baseRouter,
   userRouter,
   doctorRouter,
   adminRouter
