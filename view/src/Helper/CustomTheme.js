084248c2925584c93c35bf96541166c1280626de
unknown <belalelmeligy516@gmail.com>
2022-07-11 19:03:54 +0200

    feat: Add custom theme to customize project's color identity and typography

---
+++ view/src/Helper/CustomTheme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#3AC99E'
+        },
+        text: {
+            primary: '#2E4046',
+            secondary: '#3AC99E'
+        }
+    },
+    typography: {
+
+    }
+});
