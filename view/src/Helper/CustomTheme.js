a8c01afa6ed28854e67d4809efbee1170f43d8b6
Basem98 <basem.mostafa98@hotmail.com>
2022-07-15 22:29:53 +0200

    feat: Define color theme based on wireframe

--- view/src/Helper/CustomTheme.js
+++ view/src/Helper/CustomTheme.js
@@ -2,12 +2,30 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
     palette: {
-        primary: {
-            main: '#3AC99E'
+        primaryBg: {
+            main: '#F9F9F9',
+        },
+        secondaryBg: {
+            main: '#A6E0D6'
+        },
+        grey: {
+            main: '#D9D9D9'
         },
         text: {
             primary: '#2E4046',
-            secondary: '#3AC99E'
+            secondary: '#FFFFFF'
+        },
+        highlight: {
+            main: '#3AC99E'
+        },
+        ratingGold: {
+            main: '#FFE234'
+        },
+        linearHeroBg: {
+            main: 'linear-gradient(180deg, rgba(248, 251, 252, 0) 0%, rgba(152, 226, 205, 0.505208) 77.6%, #A6E0D6 100%)'
+        },
+        linearFormBg: {
+            main: 'linear-gradient(180deg, #21D0C3 0%, rgba(166, 224, 214, 0.25) 22.4%)'
         }
     },
     typography: {
