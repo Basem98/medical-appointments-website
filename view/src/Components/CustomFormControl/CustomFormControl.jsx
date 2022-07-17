a6a240efc2757d005e649b600c4a835137521c55
Basem98 <basem.mostafa98@hotmail.com>
2022-07-17 22:53:20 +0200

    feat: Create a custom FormControl based on wireframe style

---
+++ view/src/Components/CustomFormControl/CustomFormControl.jsx
@@ -0,0 +1,34 @@
+import { FormControl, styled } from "@mui/material";
+
+const CustomFormControl = styled(FormControl)(({ theme }) => ({
+  minHeight: 36,
+  '& label': {
+    fontSize: 16,
+    top: -10,
+    fontWeight: 'lighter'
+  },
+  '& .MuiOutlinedInput-root': {
+    borderRadius: 12,
+    backgroundColor: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'lighter',
+    '& .MuiSelect-select': {
+      height: 26,
+      padding: '5px 15px'
+    },
+    '& fieldset': {
+      borderColor: 'transparent',
+    },
+    '&:hover fieldset': {
+      borderColor: theme.palette.highlight.main,
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: theme.palette.highlight.main,
+    }
+  },
+  '& label.Mui-focused': {
+    color: `${theme.palette.text.primary} !important`,
+  }
+}));
+
+export default CustomFormControl;
