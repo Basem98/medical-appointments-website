7a934c5566a4bfc040986efc0377832799bd4811
Basem98 <basem.mostafa98@hotmail.com>
2022-07-17 13:03:51 +0200

    feat: Implement the multi-step form navigation wrapper

---
+++ view/src/Components/FormNavigator/FormNavigator.jsx
@@ -0,0 +1,27 @@
+import { Grid } from '@mui/material';
+import CustomFormButton from '../CustomFormButton/CustomFormButton';
+
+
+
+const FormNavigator = ({ hasPreviousStep, goBack, isLastStep }) => {
+  return (
+    <Grid container item xs={12} justifyContent='space-around' marginY='50px'>
+      {
+        hasPreviousStep && (
+          <Grid item xs={4}>
+            <CustomFormButton type='button' onClick={goBack} variant='outlined' fullWidth>
+              Back
+            </CustomFormButton>
+          </Grid>
+        )
+      }
+      <Grid item xs={hasPreviousStep ? 4 : 6}>
+        <CustomFormButton type="submit" variant='contained' fullWidth>
+          {isLastStep ? 'Submit' : 'Next'}
+        </CustomFormButton>
+      </Grid>
+    </Grid >
+  );
+}
+
+export default FormNavigator;
