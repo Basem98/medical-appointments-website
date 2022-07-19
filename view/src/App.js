310de1b14be55c522787ebc78fa732b7e6ff00a6
Basem98 <basem.mostafa98@hotmail.com>
2022-07-19 12:40:48 +0200

    feat: Wrap the app component in a grid container

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Grid } from '@mui/material';
+import DoctorSignUpForm from './Components/DoctorSignUpForm/DoctorSignUpForm';
+
 
 function App() {
   return (
-    <>
-      <h1>Medical Appointments Website</h1>
-    </>
+    <Grid container height='100vh' justifyContent='center' alignItems='center' sx={{backgroundColor: 'rgba(249, 249, 249, 0.5)', marginY: -1}}>
+      <Grid container item height='fit-content' xl={4} lg={6} md={6} sm={8} xs={12}>
+        <DoctorSignUpForm />
+      </Grid>
+    </Grid>
   );
 }
 
