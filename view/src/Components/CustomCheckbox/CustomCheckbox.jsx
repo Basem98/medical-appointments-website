81fc6e5fa9fa4b4815db07b464bb0fb680987005
Basem98 <basem.mostafa98@hotmail.com>
2022-07-20 13:40:39 +0200

    docs: Document the CustomCheckBox component

--- view/src/Components/CustomCheckbox/CustomCheckbox.jsx
+++ view/src/Components/CustomCheckbox/CustomCheckbox.jsx
@@ -1,7 +1,13 @@
 import { Checkbox, FormControlLabel, useTheme } from '@mui/material';
 import { useField } from 'formik';
 
-
+/**
+ * @description A custom checkbox component that extends MUI's FormControlLabel,
+ * and integrates it with Formik to be used in any form built with it
+ * @param {String} label the label to be used for the field in the form
+ * @param {String} name the name of this field in your formik's initialValues object
+ * @returns A <FormControlLabel> component that's used to create a customized Checkbox component with our wireframe's design
+ */
 const CustomCheckbox = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   const theme = useTheme();
