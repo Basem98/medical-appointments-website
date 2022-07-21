3b40316307ef2532b645fe617ec85900de133a32
unknown <belalelmeligy516@gmail.com>
2022-07-21 22:20:28 +0200

    refactor: Re-Implement User Sign up form using reusable components

--- view/src/Components/UserSignupForm/UserSignupForm.js
+++ view/src/Components/UserSignupForm/UserSignupForm.js
@@ -1,209 +1,100 @@
-import { Typography, TextField, Grid, InputAdornment, Button} from "@mui/material";
-import { ThemeProvider } from "@mui/system";
-import React from "react";
-import PopupForm from "../PopupForm/PopupForm";
-import theme from "../../Helper/CustomTheme";
-import Person from '@mui/icons-material/Person';
-import Phone from '@mui/icons-material/Phone';
-import Email from '@mui/icons-material/Email';
-import Key from '@mui/icons-material/Key';
-import IconButton from '@mui/material/IconButton';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
-
-
+import React, { useState } from "react";
 import "./UserSignupForm.css"
-import CountrySelect from "../CountrySelect/CountrySelect";
+import { Grid } from "@mui/material";
+import InputField from '../InputField/InputField'
+import { Formik, Form } from "formik";
+import theme from "../../Helper/CustomTheme";
+import CustomFormButton from "../CustomFormButton/CustomFormButton";
+import PersonIcon from '@mui/icons-material/Person';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 export default function UserSignupForm() {
-    const [values, setValues] = React.useState({
-        password: '',
-        showPassword: false,
-    });
 
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
-    };
-
-    const handleClickShowPassword = () => {
-        setValues({
-            ...values,
-            showPassword: !values.showPassword,
-        });
-    };
-
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
-    const FormContent =
-        <>
-            <ThemeProvider theme={theme}>
-                <Typography
-                    variant='h3'
-                    align='center'
-                    padding='30px'
-                    fontWeight='bold'
-                    color="textPrimary"
-                >
-                    Sign up
-                </Typography>
-
-                <Grid
-                    container
-                    spacing={2}
-                    rowSpacing={3}
-                    paddingTop='100px'
-                    margin="0 auto"
-                    direction="row"
-                    width="90%"
-                >
-                    <Grid item xs={6} width="75%">
-                        <TextField
-                            placeholder="First Name"
-                            variant="outlined"
-                            fullWidth
-                            sx={{
-                                backgroundColor: "white",
-                            }}
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <Person fontSize="large" color="primary" />
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField
-                            placeholder="Last Name"
-                            variant="outlined"
-                            fullWidth
-                            sx={{
-                                backgroundColor: "white",
-                            }}
-                        />
-                    </Grid>
-
-                    <Grid item xs={4}>
-                        <CountrySelect></CountrySelect>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <TextField
-                            placeholder="Phone Number"
-                            variant="outlined"
-                            fullWidth
-                            sx={{
-                                backgroundColor: "white",
-                            }}
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <Phone fontSize="large" color="primary" />
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <TextField
-                            placeholder="Email Address"
-                            variant="outlined"
-                            fullWidth
-                            sx={{
-                                backgroundColor: "white",
-                            }}
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <Email fontSize="large" color="primary" />
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <OutlinedInput
-                            placeholder="Password"
-                            sx={{
-                                backgroundColor: "white"
-                            }}
-                            id="outlined-adornment-password"
-                            type={values.showPassword ? 'text' : 'password'}
-                            value={values.password}
-                            fullWidth
-                            onChange={handleChange('password')}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                    >
-                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            startAdornment={
-                                <InputAdornment position="start">
-                                    <Key fontSize="large" color="primary" />
-                                </InputAdornment>
-                            }
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <OutlinedInput
-                            placeholder="Confirm Password"
-                            sx={{
-                                backgroundColor: "white"
-                            }}
-                            type={values.showPassword ? 'text' : 'password'}
-                            fullWidth
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                    >
-                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
-                    </Grid>
-
-                    <Grid item xs={6} margin="0 auto">
-                        <Button
-                            variant="contained"
-                            type="submit"
-                            fullWidth
-                            sx={{
-                                padding: "15px",
-                                backgroundColor: "primary",
-                                fontSize: "larger",
-                                fontWeight: "bold",
-                                color: "white",
-                                borderRadius: "15px"
-                            }}
-                        >
-                            Submit
-                        </Button>
-                    </Grid>
-                </Grid>
-            </ThemeProvider>
-        </>
+    const [showPassword, setShowPassword] = useState(false);
+    const handleShowPasswordToggle = () => setShowPassword(!showPassword);
 
 
     return (
         <>
-            <PopupForm
-                FormContent={FormContent}
-            >
-            </PopupForm>
+            <Grid container height='100vh' justifyContent='center' alignItems='center' sx={{ backgroundColor: 'rgba(249, 249, 249, 0.5)', marginY: -1 }}>
+                <Grid container item height='fit-content' xl={4} lg={6} md={6} sm={8} xs={12} sx={{
+                    background: 'linear-gradient(180deg, #21D0C3 0%, rgba(166, 224, 214, 0.25) 22.4%)',
+                    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+                    borderRadius: '20px'
+                }}>
+                    <Grid item sx={{ width: '100%' }}>
+                        <h2 style={{ ...theme.typography.h2, textAlign: 'center' }}>Sign Up</h2>
+                    </Grid>
+                    <Formik
+                        initialValues={{
+                            firstName: '',
+                            lastName: '',
+                            phoneNumber: '',
+                            email: '',
+                            password: '',
+                            confirmPassword: ''
+
+                        }}
+                    >
+                        {
+                            (formik) => (
+                                <Form>
+                                    <Grid container justifyContent='center' alignItems='center'>
+                                        <Grid item xs={10} md={5} sx={{ marginTop: '25px' }}>
+                                            <InputField label='First Name' name='firstName' placeholder='First Name' >
+                                                <PersonIcon></PersonIcon>
+                                            </InputField>
+                                        </Grid>
+                                        <Grid item xs={10} md={5} sx={{ marginTop: '25px', ml: '1px' }}>
+                                            <InputField label='Last Name' name='lastName' placeholder='Last Name' >
+                                                <PersonIcon></PersonIcon>
+                                            </InputField>
+                                        </Grid>
+                                        <Grid item xs={10} sx={{ marginTop: '25px' }}>
+                                            <InputField label='Phone Number' name='phoneNumber' placeholder='Phone Number'>
+                                                <PhoneIcon></PhoneIcon>
+                                            </InputField>
+                                        </Grid>
+                                        <Grid item xs={10} sx={{ marginTop: '25px' }}>
+                                            <InputField label='Email' name='email' placeholder='Enter Your Email'>
+                                                <EmailIcon></EmailIcon>
+                                            </InputField>
+                                        </Grid>
+                                        <Grid item xs={10} sx={{ marginTop: '25px' }}>
+                                            <InputField label='Password' name='password' placeholder='Password'
+                                                type={showPassword ? "text" : "password"}
+                                            >
+                                                <VpnKeyIcon></VpnKeyIcon>
+                                                {showPassword ?
+                                                    <VisibilityOffIcon onClick={handleShowPasswordToggle} cursor='pointer' /> :
+                                                    <VisibilityIcon onClick={handleShowPasswordToggle} cursor='pointer' />}
+                                            </InputField>
+                                        </Grid>
+                                        <Grid item xs={10} sx={{ marginTop: '25px' }}>
+                                            <InputField label='Confirm Password' name='confirmPassword' placeholder='Confirm Password'
+                                                type={showPassword ? "text" : "password"}
+                                            >
+                                                <VpnKeyIcon></VpnKeyIcon>
+                                                {showPassword ?
+                                                    <VisibilityOffIcon onClick={handleShowPasswordToggle} cursor='pointer' /> :
+                                                    <VisibilityIcon onClick={handleShowPasswordToggle} cursor='pointer' />}
+                                            </InputField>
+                                        </Grid>
+                                        <Grid item xs={8} sx={{ marginTop: '25px' }}>
+                                            <CustomFormButton variant="contained" type="submit" xs={8} sx={{ width: '100%', mb: '50px' }}>Submit</CustomFormButton>
+                                        </Grid>
+                                    </Grid>
+                                </Form>
+                            )
+                        }
+
+                    </Formik>
+                </Grid>
+            </Grid>
         </>
     );
 }
