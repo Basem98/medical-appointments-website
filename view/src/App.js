bb6b3d92bb03143cf898a460ec51097b80e71394
unknown <belalelmeligy516@gmail.com>
2022-07-23 14:48:34 +0200

    Revert "chore: running npm i to install dependencies"
    This reverts commit 7b78c5ca6927f9814a389e2c9532cf331732bdfe.

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { Grid } from '@mui/material';
-import UserSignupForm from './Components/UserSignupForm/UserSignupForm'
+import DoctorSignUpForm from './Components/DoctorSignUpForm/DoctorSignUpForm';
+
 
 function App() {
   return (
-    <UserSignupForm />
+    <Grid container height='100vh' justifyContent='center' alignItems='center' sx={{backgroundColor: 'rgba(249, 249, 249, 0.5)', marginY: -1}}>
+      <Grid container item height='fit-content' xl={4} lg={6} md={6} sm={8} xs={12}>
+        <DoctorSignUpForm />
+      </Grid>
+    </Grid>
   );
 }
 
