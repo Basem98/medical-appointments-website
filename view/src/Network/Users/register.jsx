f552e331376a2aed49d09906087dc54915ac58f1
unknown <belalelmeligy516@gmail.com>
2022-07-23 18:04:48 +0200

    refactor: Edit console log message to output message from backend instead of the whole axios message

--- view/src/Network/Users/register.jsx
+++ view/src/Network/Users/register.jsx
@@ -3,7 +3,7 @@ import axiosClient from "../axiosClient";
 const submitUserData = (userData) => {
     return axiosClient.post('/api/users', userData)
         .then((response) => console.log(response))
-        .catch(err => console.log(err))
+        .catch(err => console.log(err.response.data))
 }
 
 export default submitUserData;
