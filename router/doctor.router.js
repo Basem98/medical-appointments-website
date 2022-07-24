ad62425482e8c94705ed09b93b6388b5a79d406c
Basem98 <basem.mostafa98@hotmail.com>
2022-07-24 19:10:56 +0200

    feat: Implement /api/doctors/upload/images POST endpoint to upload images

--- router/doctor.router.js
+++ router/doctor.router.js
@@ -3,10 +3,13 @@ const doctorRouter = express.Router();
 const { validateUserData } = require('../middleware/doctor/signup.middleware');
 const validationResult = require('../middleware/user/validation.middleware');
 const { signUp } = require('../controller/doctors/main.controller');
-const {verifyEmail} = require('../middleware/verification.middleware');
+const { verifyEmail } = require('../middleware/verification.middleware');
 const { isAlreadyInDb } = require('../middleware/doctor/exists.middleware');
-
+const { uploadImages } = require('../controller/doctors/files.controller');
+const multerUpload = require('../middleware/multer.middleware');
+const bufferFileToString = require('../middleware/bufToString.middleware');
 
 doctorRouter.post('/', validateUserData(), validationResult, isAlreadyInDb, signUp, verifyEmail);
+doctorRouter.post('/upload/images', multerUpload.array('images', 2), bufferFileToString, uploadImages);
 
 module.exports = doctorRouter;
