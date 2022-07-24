ccc15c1a8a188a5e6296e95734de51b28150584e
Basem98 <basem.mostafa98@hotmail.com>
2022-07-24 19:06:21 +0200

    feat: Implement dataUri mw to convert image buffers to strings

---
+++ middleware/bufToString.middleware.js
@@ -0,0 +1,23 @@
+const DataUri = require('datauri/parser');
+const dUri = new DataUri();
+
+const formatBufferUsingExt = (file) => dUri.format(file.originalname, file.buffer);
+
+const bufferFileToString = (req, res, next) => {
+  try {
+    console.log(req);
+    if (req.file) {
+      req.file = formatBufferUsingExt(req.file).content;
+    } else if (req.files.length > 0) {
+      req.files = req.files.map(file => formatBufferUsingExt(file).content)
+    } else {
+      throw new Error('There are no files to upload');
+    }
+    next();
+  } catch (err) {
+    err.statusCode = 500;
+    next(err);
+  }
+}
+
+module.exports = bufferFileToString;
