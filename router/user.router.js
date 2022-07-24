addb198bae1cbc1aca8e528a3e784e4fa1c31500
unknown <belalelmeligy516@gmail.com>
2022-07-25 01:07:25 +0200

    feat: Add checkDuplicate middleware to the user sign up endpoint in users route

--- router/user.router.js
+++ router/user.router.js
@@ -2,12 +2,13 @@ const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controller/users/main.controller');
 const authorizationMiddleware = require('../middleware/user/authorization.middleware');
+const checkDuplicate = require('../middleware/user/checkDuplicate.middleware');
 const validationMiddleware = require('../middleware/user/validation.middleware');
 const { verifyEmail } = require('../middleware/verification.middleware');
 
 
 userRouter.route('/')
-    .post(userController.validateUserData(), validationMiddleware, userController.addUser, verifyEmail)
+    .post(userController.validateUserData(), validationMiddleware,checkDuplicate, userController.addUser, verifyEmail)
 
 userRouter.route('/login')
     .post(userController.loginUser)
