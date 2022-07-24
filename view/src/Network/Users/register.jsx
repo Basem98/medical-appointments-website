3b5831005fc4394b8059d3681fa6acad7d9a2869
unknown <belalelmeligy516@gmail.com>
2022-07-25 01:46:30 +0200

    refactor: Make the submitUserData function returns only a promise and Moved the .then() .catch() parts to the user sign up component

--- view/src/Network/Users/register.jsx
+++ view/src/Network/Users/register.jsx
@@ -2,8 +2,6 @@ import axiosClient from "../axiosClient";
 
 const submitUserData = (userData) => {
     return axiosClient.post('/api/users', userData)
-        .then((response) => console.log(response))
-        .catch(err => console.log(err.response.data))
 }
 
 export default submitUserData;
