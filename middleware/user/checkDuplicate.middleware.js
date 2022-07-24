1efc3129912b79c6186f09bbd3f6e953fd84cdf2
unknown <belalelmeligy516@gmail.com>
2022-07-25 01:05:58 +0200

    feat: Add middleware to check duplication in email or phone number in user sign up stage

---
+++ middleware/user/checkDuplicate.middleware.js
@@ -0,0 +1,32 @@
+const User = require('../../model/user.model');
+
+const checkDuplicate = async (req, res, next) => {
+    let enteredEmail = req.body.email;
+    let enteredPhoneNumber = req.body.phoneNumber;
+
+    // Check Duplication for Phone Number
+    await User.findOne({ phoneNumber: enteredPhoneNumber }).count()
+        .then((count) => {
+            if (count == 1) {
+                let error = new String("The provided Phone Number is already registered");
+                error.statusCode = 400;
+                next(error);
+            }
+        })
+        .catch((error) => console.log(error))
+    next();
+
+    // Check Duplication for email
+    await User.findOne({ email: enteredEmail }).count()
+        .then((count) => {
+            if (count == 1) {
+                let error = new String("The provided Email is already registered");
+                error.statusCode = 400;
+                next(error);
+            }
+        })
+        .catch(error => console.log(error))
+
+}
+
+module.exports = checkDuplicate;
