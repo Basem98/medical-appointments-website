623219c0458d40159e82bd93438a30d2821c5c29
unknown <belalelmeligy516@gmail.com>
2022-07-26 06:23:46 +0200

    refactor: Make main file responsible only for containing controller functions (import and export)

--- controller/doctors/main.controller.js
+++ controller/doctors/main.controller.js
@@ -1,49 +1,9 @@
-const { default: mongoose } = require('mongoose');
-const Doctor = require('../../model/doctor.model');
-
-
-async function signUp(req, res, next) {
-  try {
-    const newDoctor = new Doctor(req.body);
-    if (!newDoctor) {
-      throw new Error('Failed to create a new document');
-    }
-    await newDoctor.save();
-    req.body.id = newDoctor._id;
-    req.body.role = 'Doctor';
-    next();
-  } catch (err) {
-    console.error(err);
-    err.statusCode = 500;
-    next(err);
-  }
-}
-
-
-const getDoctorById = async (req, res, next) => {
-  let doctorId = req.params.id;
-  if (!mongoose.isValidObjectId(doctorId)) {
-    return res.status(404).json({ message: 'Doctor is Not Found' });
-  }
-  await Doctor.findOne({ _id: doctorId })
-    .then((data) => {
-      if (data) {
-        // Execluding password from the retrieved data
-        data.password = undefined;
-        return res.status(200).json({ message: data })
-      }
-      return res.status(404).json({ message: 'Doctor is Not Found' });
-    })
-    .catch(error => {
-      console.log(error);
-      error.statusCode = 500;
-      next(error);
-    })
-}
-
-
+const signUp = require('./signup.controller');
+const getDoctorById = require('./getDoctorById.controller');
+const {uploadImages} = require('./files.controller');
 
 module.exports = {
   signUp,
   getDoctorById,
+  uploadImages
 }
