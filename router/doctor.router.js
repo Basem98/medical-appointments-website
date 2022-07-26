d43a313ed49e39dc0894cb8e92dac1bc9de75b3f
unknown <belalelmeligy516@gmail.com>
2022-07-26 07:25:37 +0200

    refactor: Import doctors controller functions from the same main.controller file

--- router/doctor.router.js
+++ router/doctor.router.js
@@ -2,10 +2,9 @@ const express = require('express');
 const doctorRouter = express.Router();
 const { validateUserData } = require('../middleware/doctor/signup.middleware');
 const validationResult = require('../middleware/user/validation.middleware');
-const { signUp, getDoctorById } = require('../controller/doctors/main.controller');
+const { signUp, getDoctorById, uploadImages } = require('../controller/doctors/main.controller');
 const { verifyEmail } = require('../middleware/verification.middleware');
 const { isAlreadyInDb } = require('../middleware/doctor/exists.middleware');
-const { uploadImages } = require('../controller/doctors/files.controller');
 const multerUpload = require('../middleware/multer.middleware');
 const bufferFileToString = require('../middleware/bufToString.middleware');
 
