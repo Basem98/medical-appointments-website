e5f314911fd2e59d7fc6b4739067ad9888309f61
Basem98 <basem.mostafa98@hotmail.com>
2022-07-27 14:01:03 +0200

    feat: Handle sign up server response

--- view/src/Components/DoctorSignUpForm/DoctorSignUpForm.jsx
+++ view/src/Components/DoctorSignUpForm/DoctorSignUpForm.jsx
@@ -6,6 +6,7 @@ import CertificationsFormStep from "../CertificationsFormStep/CertificationsForm
 import ClinicsFormStep from "../ClinicsFormStep/ClinicsFormStep";
 import UploadFormStep from "../UploadFormStep/UploadFormStep";
 import initialDoctorFormValues from "../../Helper/InitialDocFormValues";
+import CustomAlert from '../CustomAlert/CustomAlert';
 import {
   personalFormStepValidation,
   educationFormStepValidation,
@@ -15,10 +16,12 @@ import {
   imagesFormStepValidation
 } from '../../Helper/ValidationSchema';
 import submitDoctorApplication from "../../Network/Doctors/register";
+import { useState } from "react";
 
 
 
 const DoctorSignUpForm = () => {
+  const [serverResponse, setServerResponse] = useState({ success: false, msg: '' });
   const handleFormSubmit = (values) => {
     /* Create a deep copy of all the reference types inside the values argument to avoid mutations */
     let doctorApplicationData = {
@@ -51,37 +54,49 @@ const DoctorSignUpForm = () => {
     /* Make sure the certifications field is deleted if it's empty to pass the backend validation layer */
     if (!doctorApplicationData.certifications) delete doctorApplicationData.certifications;
     /* Make the axios calls to submit the doctor's application to the backend */
-    submitDoctorApplication(doctorApplicationData);
+    submitDoctorApplication(doctorApplicationData)
+      .then(response => {
+        if (response?.status >= 200 && response?.status < 300)
+          setServerResponse({ success: true, msg: 'Congratulations, your application has been submitted! One of our professionals will review your application and verify your data asap.' });
+        else
+          throw new Error('Something went wrong! Please try again, or contact us to help you solve the problem.')
+      })
+      .catch(err => {
+        setServerResponse({ success: false, msg: 'Something went wrong! Please try again, or contact us to help you solve the problem.' });
+      });
   }
 
-
   return (
-    <MultiStepForm initialValues={initialDoctorFormValues} onSubmit={handleFormSubmit}>
-      <PersonalFormStep
-        stepName="Personal"
-        validationSchema={personalFormStepValidation}
-      />
-      <EducationFormStep
-        stepName="Qualifications"
-        validationSchema={educationFormStepValidation}
-      />
-      <CertificationsFormStep
-        stepName={`Certifications\n(optional)`}
-        validationSchema={certificationsFormStepValidation}
-      />
-      <ExperienceFormStep
-        stepName="Experience"
-        validationSchema={experienceFormStepValidation}
-      />
-      <ClinicsFormStep
-        stepName="Clinics"
-        validationSchema={clinicsFormStepValidation}
-      />
-      <UploadFormStep
-        stepName="File Upload"
-        validationSchema={imagesFormStepValidation}
-      />
-    </MultiStepForm >
+    <>
+      <MultiStepForm
+        initialValues={initialDoctorFormValues} onSubmit={handleFormSubmit}
+        serverResponse={serverResponse} setServerResponse={(newResponse) => setServerResponse(newResponse)}>
+        <PersonalFormStep
+          stepName="Personal"
+          validationSchema={personalFormStepValidation}
+        />
+        <EducationFormStep
+          stepName="Qualifications"
+          validationSchema={educationFormStepValidation}
+        />
+        <CertificationsFormStep
+          stepName={`Certifications\n(optional)`}
+          validationSchema={certificationsFormStepValidation}
+        />
+        <ExperienceFormStep
+          stepName="Experience"
+          validationSchema={experienceFormStepValidation}
+        />
+        <ClinicsFormStep
+          stepName="Clinics"
+          validationSchema={clinicsFormStepValidation}
+        />
+        <UploadFormStep
+          stepName="File Upload"
+          validationSchema={imagesFormStepValidation}
+        />
+      </MultiStepForm >
+    </>
   );
 }
 
