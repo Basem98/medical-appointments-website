11f7335d559f598e88771b923abd83a44da02892
Basem98 <basem.mostafa98@hotmail.com>
2022-07-27 13:54:04 +0200

    feat: Implement the /api/doctors/validate/ POST routes to validate unique data

--- router/doctor.router.js
+++ router/doctor.router.js
@@ -8,8 +8,11 @@ const { isAlreadyInDb } = require('../middleware/doctor/exists.middleware');
 const { uploadImages } = require('../controller/doctors/files.controller');
 const multerUpload = require('../middleware/multer.middleware');
 const bufferFileToString = require('../middleware/bufToString.middleware');
+const { isEmailAlreadyInDb, isPhoneAlreadyInDb } = require('../controller/doctors/exists.controller');
 
 doctorRouter.post('/', validateUserData(), validationResult, isAlreadyInDb, signUp, verifyEmail);
 doctorRouter.post('/upload/images', multerUpload.array('images', 2), bufferFileToString, uploadImages);
+doctorRouter.post('/validate/email', isEmailAlreadyInDb);
+doctorRouter.post('/validate/phone', isPhoneAlreadyInDb);
 
 module.exports = doctorRouter;
