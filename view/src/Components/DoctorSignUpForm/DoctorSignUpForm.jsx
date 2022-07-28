d7d6ee97c19db3bafbcbef4969cbb9b142d2d906
Basem98 <basem.mostafa98@hotmail.com>
2022-07-28 13:04:27 +0200

    feat: Finalize initial fully-fledged version of the doctor's application

--- view/src/Components/DoctorSignUpForm/DoctorSignUpForm.jsx
+++ view/src/Components/DoctorSignUpForm/DoctorSignUpForm.jsx
@@ -16,42 +16,14 @@ import {
 } from '../../Helper/ValidationSchema';
 import submitDoctorApplication from "../../Network/Doctors/register";
 import { useState } from "react";
+import handleFormDataBeforeSub from "../../Helper/DoctorAppSubmission";
 
 
 
 const DoctorSignUpForm = () => {
   const [serverResponse, setServerResponse] = useState({ success: false, msg: '' });
   const handleFormSubmit = (values) => {
-    /* Create a deep copy of all the reference types inside the values argument to avoid mutations */
-    let doctorApplicationData = {
-      ...values,
-      experiences: values.experiences.map(elm => ({ ...elm })),
-      education: values.education.map(elm => ({ ...elm })),
-      certifications: values.certifications.map(elm => ({ ...elm })),
-      clinics: values.clinics.map(elm => ({ ...elm }))
-    };
-    /* Make sure the dates in side the experiences field are converted into a JS Date object */
-    doctorApplicationData.experiences = doctorApplicationData.experiences.map(exp => {
-      let newExpObj = exp;
-      newExpObj.startDate = new Date(newExpObj.startDate.year, newExpObj.startDate.month);
-      newExpObj.endDate = !newExpObj.isCurrentlyWorking ? new Date(newExpObj.endDate.year, newExpObj.endDate.month) : '';
-      return newExpObj;
-    });
-    /* Make sure the dates in side the education field are converted into a JS Date object */
-    doctorApplicationData.education = doctorApplicationData.education.map(eduObj => {
-      let newEduObj = eduObj;
-      newEduObj.issueDate = new Date(newEduObj.issueDate.year, newEduObj.issueDate.month);
-      return newEduObj;
-    });
-    /* Make sure the dates in side the certifications field are converted into a JS Date object */
-    doctorApplicationData.certifications = doctorApplicationData.certifications && doctorApplicationData.certifications[0]?.title ?
-      doctorApplicationData.certifications.map(certObj => {
-        let newCertObj = certObj;
-        newCertObj.issueDate = new Date(newCertObj.issueDate.year, newCertObj.issueDate.month);
-        return newCertObj;
-      }) : null;
-    /* Make sure the certifications field is deleted if it's empty to pass the backend validation layer */
-    if (!doctorApplicationData.certifications) delete doctorApplicationData.certifications;
+    const doctorApplicationData = handleFormDataBeforeSub(values);
     /* Make the axios calls to submit the doctor's application to the backend */
     submitDoctorApplication(doctorApplicationData)
       .then(response => {
