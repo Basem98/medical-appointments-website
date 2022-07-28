8eb25f70b6a07d9615777ce1ff6a0ea352041656
unknown <belalelmeligy516@gmail.com>
2022-07-29 01:49:29 +0200

    feat: Implement basic functionality to fetch upcoming appointments based on userId or doctorId

--- controller/appointment/getUpcomingsById.controller.js
+++ controller/appointment/getUpcomingsById.controller.js
@@ -1,7 +1,25 @@
-const User = require('../../model/user.model');
+const { default: mongoose } = require('mongoose');
+const Appointment = require('../../model/appointment.model');
 
 const getUpcomingsById = (req, res, next) => {
-    
+    Appointment.find({
+        "$or": [
+            {'time.userId': req.params.id},
+            {'time.doctorId': req.params.id}
+        ]
+    })
+    .populate('time.userId')
+    .populate('time.doctorId')
+    .then((data) => {
+        if(!data) {
+            return res.status(404).send({message: 'Appointment Not Found'});
+        }
+        res.status(200).json({message: data})
+    })
+    .catch(error => {
+        error.statusCode = 500
+        next(error);
+    })
 }
 
 module.exports={getUpcomingsById};
