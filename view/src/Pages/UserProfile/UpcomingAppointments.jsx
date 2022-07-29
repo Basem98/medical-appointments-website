d4deb7572a0775c8d756ce780bba5506924a7d51
unknown <belalelmeligy516@gmail.com>
2022-07-29 18:22:19 +0200

    refactor: Add optional chaining to upcomings vairable

--- view/src/Pages/UserProfile/UpcomingAppointments.jsx
+++ view/src/Pages/UserProfile/UpcomingAppointments.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 const UpcomingAppointments = ({ upcomings }) => {
 
-    return upcomings.map((upcoming, index) => {
+    return upcomings?.map((upcoming, index) => {
         return (
             <div key={index}>{upcoming.date}</div>
         );
