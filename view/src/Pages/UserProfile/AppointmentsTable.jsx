806b3838e903327be511fdba9fbf2ce045c6b610
unknown <belalelmeligy516@gmail.com>
2022-07-29 23:01:24 +0200

    feat: Implement Styling and appropriate data displays for appointment cards

--- view/src/Pages/UserProfile/AppointmentsTable.jsx
+++ view/src/Pages/UserProfile/AppointmentsTable.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Card, CardActions, CardContent, Grid, Typography } from "@mui/material";
-import {useTheme }from "@mui/material";
+import { Card, CardActions, CardContent, Grid, Typography, Link, Button } from "@mui/material";
+import { useTheme } from "@mui/material";
+import moment from "moment";
+
 const AppointmentsTable = ({ appointments }) => {
     const theme = useTheme();
     return (
@@ -12,17 +14,76 @@ const AppointmentsTable = ({ appointments }) => {
         >
             {
                 appointments?.map((appointment, index) => {
-                    console.log(appointment.doctor.firstName)
                     return (
-                        <Grid item key={index} xs={12} sm={10}>
-                            <Card sx={{boxShadow: `1px 1px 1px 1px ${theme.palette.highlight.main}`}}>
-                                <CardContent>
-                                    <Typography variant="body1">{appointment.date}</Typography>
-                                </CardContent>
-
-                                <CardActions>
-                                    
-                                </CardActions>
+                        <Grid item key={index} xs={12} md={10}>
+                            <Card sx={{ boxShadow: `1px 1px 1px 1px ${theme.palette.highlight.main}` }}>
+                                <Grid container>
+                                    <Grid item width="70%">
+                                        <CardContent>
+                                            <Grid>
+                                                <Typography display="inline" variant="h4">
+                                                    {moment(appointment.date.split('T')[0]).format('dddd, MMMM Do YYYY')}
+                                                </Typography>
+                                                <Typography
+                                                    display="inline"
+                                                    color={theme.palette.grey[500]}
+                                                > at {appointment.time.hour}
+                                                    : {appointment.time.minute}
+                                                </Typography>
+                                            </Grid>
+                                            <Typography
+                                                variant="body2"
+                                                color={theme.palette.grey[500]}
+                                            >
+                                                Lasts for {appointment.time.duration} minutes
+                                            </Typography>
+                                            <Typography
+                                                color={theme.palette.grey[500]}
+                                                marginTop={3}
+                                            >
+                                                {
+                                                    moment(appointment.date).fromNow()
+                                                }
+                                            </Typography>
+                                            <Typography
+                                                marginTop={10}
+                                            >
+                                                <>With </>
+                                                <Link
+                                                    sx={{
+                                                        color: theme.palette.highlight.main,
+                                                        '&:hover': {
+                                                            cursor: 'pointer'
+                                                        }
+                                                    }}
+                                                    href='#'
+                                                >Dr. {appointment.doctor.firstName} {appointment.doctor.lastName}
+                                                </Link>
+                                            </Typography>
+                                        </CardContent>
+                                    </Grid>
+                                    <Grid item alignSelf="center" width="25%">
+                                        <CardActions>
+                                            <Button
+                                                sx={{
+                                                    backgroundColor: theme.palette.highlight.main,
+                                                    fontWeight: 'bold',
+                                                    padding: "15px",
+                                                    borderRadius: "20px",
+                                                    width: "100%",
+                                                    ':hover': {
+                                                        backgroundColor: theme.palette.highlight.main,
+                                                        opacity: 0.8,
+                                                        transform: 'scale(1.05)'
+                                                    }
+                                                }}
+                                                variant="contained"
+                                            >
+                                                See Details
+                                            </Button>
+                                        </CardActions>
+                                    </Grid>
+                                </Grid>
                             </Card>
                         </Grid>
                     );
@@ -30,8 +91,6 @@ const AppointmentsTable = ({ appointments }) => {
             }
         </Grid>
     );
-
-
 }
 
 export default AppointmentsTable;
