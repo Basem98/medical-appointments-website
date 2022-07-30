c801f3239049764c4d63cdea44b6844cc7e50223
unknown <belalelmeligy516@gmail.com>
2022-07-31 00:52:12 +0200

    docs: Define the appointment schema model based on db model

---
+++ docs/models/appointment.js
@@ -0,0 +1,68 @@
+const { password, ...userData } = require("./user");
+const { password, ...doctorData } = require("./doctor");
+
+module.exports = {
+    required: [
+        "date",
+        "time"
+    ],
+    schema: {
+        "date": {
+            "type": "string",
+            "format": "date",
+            "description": "The day, month, and year of the appointment"
+        },
+        "time": {
+            "type": "object",
+            "description": "The hour, minute, and duration of the appointment",
+            "properties": {
+                "hour": {
+                    "type": "integer",
+                },
+                "minute": {
+                    "type": "integer",
+                },
+                "duration": {
+                    "type": "integer",
+                }
+            }
+        },
+        "user": {
+            "type": "object",
+            "description": "Object contains the data of the user who booked the appointment",
+            "properties": userData
+        },
+        "doctor": {
+            "type": "object",
+            "description": "Object contains the data of the doctor who will examiner doctor",
+            "properties": doctorData
+        },
+        "info": {
+            "type": "object",
+            "description": "Object contains additional data about the appointment",
+            "properties": {
+                "prescription": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "drugName": {
+                                "type": "string"
+                            },
+                            "dosage": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                },
+                "diagnosis": {
+                    "type": "string"
+                },
+                "nextVisit": {
+                    "type": "string",
+                    "description": "Object Id refers to the next (follow up) appointment"
+                }
+            }
+        }
+    }
+}
