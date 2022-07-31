b0ee4ed262929cf7bc5569ca8c9e66c6c2c5414a
unknown <belalelmeligy516@gmail.com>
2022-07-31 12:50:02 +0200

    feat: Fetch previous appointments and render them in previous appointments section

--- view/src/Pages/UserProfile/Appointments.jsx
+++ view/src/Pages/UserProfile/Appointments.jsx
@@ -3,20 +3,26 @@ import ContentToggler from "../../Components/ContentToggler/ContentToggler";
 import { useState } from "react";
 import { useEffect } from "react";
 import getUpcomings from "../../Network/Users/getUpcomings";
+import getPrevious from "../../Network/Users/getPrevious";
 
 const Appointments = () => {
-    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZGY3MDE0ODc4MTIyNjU5NzcxMDA3ZSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjU5MTA4NjExLCJleHAiOjE2NTkxOTUwMTF9.0W0n9b4h5ZOnMwQ8tNbb7If75t8Obq6ue8-jKwweg4I";
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTYzODg3MWEzNjRjZTYwZDVhOWI4NyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjU5MjU0OTQ3LCJleHAiOjE2NTkzNDEzNDd9.8w83p9LMzNyRakF5YnuekjriLRk31WUrrR7xBg0RME8";
+
     const [upcomingAppointments, setUpcomingAppointments] = useState();
+    const [previousAppointments, setPreviousAppointments] = useState();
+
     const titles = [
         "Upcoming Appointments",
-        "Past Appointments",
-        "Cancelled Appointments"
+        "Past Appointments"
     ]
     const data = [
-        upcomingAppointments
+        upcomingAppointments,
+        previousAppointments
     ];
+
+
     useEffect(() => {
-        getUpcomings("62df7014878122659771007e", token)
+        getUpcomings("62e638871a364ce60d5a9b87", token)
             .then((response) => {
                 setUpcomingAppointments(response.data.message);
             })
@@ -24,6 +30,18 @@ const Appointments = () => {
                 console.log(error);
             })
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    useEffect(() => {
+        getPrevious("62e638871a364ce60d5a9b87", token)
+            .then((response) => {
+                setPreviousAppointments(response.data.message);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+
     return (
         <>
             <ContentToggler data={data} titles={titles} role="user"/>
