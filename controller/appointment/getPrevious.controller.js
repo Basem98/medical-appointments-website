ca9fbad6438fa2046bde186d8773247c71731d8e
unknown <belalelmeligy516@gmail.com>
2022-07-31 11:01:41 +0200

    feat: Implement getPrevious controller method to get past appointments based on given id (user's id or doctor's id)

---
+++ controller/appointment/getPrevious.controller.js
@@ -0,0 +1,34 @@
+const Appointment = require("../../model/appointment.model");
+const mongoose = require("mongoose");
+
+const getPrevious = (req, res, next) => {
+    let currentDate = new Date().toISOString();
+    console.log(req.params.id)
+    if (!mongoose.isValidObjectId(req.params)) {
+        return res.status(404).send({ message: 'Appointment Not Found' });
+    }
+    if (req.body.id != req.params.id.toString()) {
+        return res.status(401).send({ message: 'Not Authorized to see this appointment' });
+    }
+    Appointment.find({
+        "$or": [
+            { user: req.params.id },
+            { doctor: req.params.id }
+        ],
+        date: { "$lt": currentDate }
+    })
+        .populate('user')
+        .populate('doctor')
+        .then((data) => {
+            if (!data) {
+                return res.status(404).send({ message: 'Appointment Not Found' });
+            }
+            res.status(200).json({ message: data })
+        })
+        .catch(error => {
+            error.statusCode = 500
+            next(error);
+        })
+}
+
+module.exports = { getPrevious }
