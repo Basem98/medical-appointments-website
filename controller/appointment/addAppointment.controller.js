2087e43012479c1f364000c489f9b9c69c414320
Basem98 <basem.mostafa98@hotmail.com>
2022-08-01 21:11:05 +0200

    refactor: Edit the addAppointments controller to match the new schemas

--- controller/appointment/addAppointment.controller.js
+++ controller/appointment/addAppointment.controller.js
@@ -2,39 +2,21 @@ const Appointment = require('../../model/appointment.model');
 const User = require('../../model/user.model');
 const Doctor = require('../../model/doctor.model');
 
-const addAppointment = (req, res, next) => {
-    if(req.body.role === 'user') {
-        req.body.user = req.body.id;
-    } else if (req.body.role === 'doctor') {
-        req.body.doctor = req.body.id;
+const addAppointment = async (req, res, next) => {
+    try {
+        const data = req.body;
+        let newAppointment = new Appointment(data);
+        let doctor = newAppointment.doctor;
+        let doctorData = await Doctor.findById(doctor);
+        const appointmentRef = { _id: newAppointment._id, date: newAppointment.date };
+        doctorData.appointments.push(appointmentRef);
+        await newAppointment.save();
+        await doctorData.save();
+        return res.status(201).json({ message: `Appointment Saved Successfully at ${newAppointment.date}`, data: newAppointment })
+    } catch (err) {
+        err.statusCode = 500;
+        next(err);
     }
-    let newAppointment = new Appointment(req.body);
-
-    newAppointment.save()
-        .then((response) => {
-            let user = response.user;
-            let doctor = response.doctor;
-
-            User.findByIdAndUpdate(user, { $push: { appointments: newAppointment._id } })
-                .then()
-                .catch((error) => {
-                    error.statusCode = 500;
-                    next(error);
-                });
-
-            Doctor.findByIdAndUpdate(doctor, { $push: { appointments: newAppointment._id } })
-            .then()
-            .catch((error) => {
-                error.statusCode = 500;
-                next(error);
-            })
-
-            return res.status(201).json({ message: `Appointment Saved Successfully at ${response.date}`, data: newAppointment})
-        })
-        .catch(error => {
-            error.statusCode = 500;
-            next(error);
-        })
 }
 
 module.exports = { addAppointment }
