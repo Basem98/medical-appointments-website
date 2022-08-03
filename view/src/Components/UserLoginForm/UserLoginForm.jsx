0b35bce9941ddae0e6bed5a3aaf5d204881f6f5e
Basem98 <basem.mostafa98@hotmail.com>
2022-08-03 21:22:54 +0200

    feat: Create a radio button to toggle between a doctor/user sign in

--- view/src/Components/UserLoginForm/UserLoginForm.jsx
+++ view/src/Components/UserLoginForm/UserLoginForm.jsx
@@ -1,27 +1,27 @@
-import { Checkbox, FormGroup, Grid, FormControlLabel, Link, Button } from '@mui/material';
+import { CircularProgress, FormGroup, Grid, IconButton, InputLabel, Link, Typography } from '@mui/material';
 import { Form, Formik } from 'formik'
 import { useTheme } from '@mui/material';
 import InputField from '../InputField/InputField';
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 import VpnKeyRoundedIcon from '@mui/icons-material/VpnKeyRounded';
-import VisibilityRoundedIcon from '@mui/icons-material/VisibilityRounded';
-import VisibilityOffRoundedIcon from '@mui/icons-material/VisibilityOffRounded';
+import { VisibilityRounded, VisibilityOffRounded, AccountCircleRounded, LocalHospitalRounded } from '@mui/icons-material';
 import CustomFormButton from '../CustomFormButton/CustomFormButton';
 import * as Yup from 'yup'
 import React from 'react';
 import { useState } from 'react';
 import CustomCheckbox from '../CustomCheckbox/CustomCheckbox';
-import axios from 'axios'
 import submitUserData from '../../Network/Users/userLogin';
+import CustomAlert from '../CustomAlert/CustomAlert';
 
-const User_Login_url ='/userlogin'
 
 const UserSignInForm = () => {
-    const [isVisible, setVisibility] = useState(false)
+    const [isVisible, setVisibility] = useState(false);
+    const [role, setRole] = useState('user');
+    const [serverResponse, setServerResponse] = useState({ success: false, msg: '' });
+    const [formSubmitted, setFormSubmitted] = useState(false);
     const handlePass = () => {
         if (isVisible) {
             setVisibility(false)
-
         }
         else {
             setVisibility(true)
@@ -30,32 +30,87 @@ const UserSignInForm = () => {
     const initialValues = {
         email: "",
         password: "",
-        rememberMe:false
+        rememberMe: false,
+        role: "user"
     };
-    const validationSchema=Yup.object({
+    const validationSchema = Yup.object({
         email: Yup.string().email('You must provide a valid email (example@organization.domain)').required('This field is required'),
         password: Yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/g, { message: 'Your password should be at least 8 characters long and it should consist of at least one uppercase letter, one lowercase letter, one number, and one symbol' }).required('This field is required')
     });
-    const onSubmit = async (values)=>{
-        const email = values.email
-        const password = values.password
-        await submitUserData({email,password})
-        .then((res)=>{console.log(res.data.token)})
-        .catch(err=>console.log(err))
+    const onSubmit = (values) => {
+        setFormSubmitted(true);
+        submitUserData(values)
+            .then(async (res) => {
+                setServerResponse({ success: true, msg: res.data.message });
+            })
+            .catch(err => {
+                setServerResponse({ success: false, msg: err.response.data.message || err.response.data.error });
+            })
     }
 
-    const Theme = useTheme()
+    const theme = useTheme()
     return (
-        <Grid container style={{ background: Theme.palette.linearFormBg.main, borderRadius: 20, boxShadow: Theme.shadows[5] }}>
-            <Grid xs={12} item style={{ ...Theme.typography.h2, color: Theme.palette.text.primary, textAlign: 'center' }}>
-                <h2 style={{ textAlign: 'center' }}>Log in</h2>
+        <Grid container style={{ background: theme.palette.linearFormBg.main, borderRadius: 20, boxShadow: theme.shadows[5] }} justifyContent='center'>
+            <Grid item xs={12} justifyContent='center'>
+                <Typography variant='h2' style={{ color: theme.palette.text.primary, textAlign: 'center' }}>Log In</Typography>
             </Grid>
             <Formik initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}>
-                <Form>
-                    <Grid container item justifyContent={'center'}>
-                        <Grid item xs={6} marginTop='25px' marginLeft='1px' >
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}>
+                {(formik) => (<Form>
+                    <Grid container item justifyContent='center'>
+                        <Grid container item xs={8} md={7} justifyContent='space-evenly'>
+                            <Grid item xs={4} justifyContent='center'>
+                                <InputLabel
+                                    htmlFor='userRadio'
+                                    sx={{ whiteSpace: 'break-spaces', verticalAlign: 'top', textAlign: 'center', fontSize: '16px', marginY: '10px' }}>
+                                    <p style={{ margin: 0 }}>As</p> a user</InputLabel>
+                                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                    <IconButton color="highlight" component='label'
+                                        sx={{ borderBottom: role === 'user' ? `1px solid ${theme.palette.highlight.main}` : 'none' }}
+                                        onClick={() => {
+                                            setRole('user');
+                                            formik.setFieldValue('role', 'user');
+                                        }}>
+                                        <input type="radio" name="role" value="user" id="userRadio" style={{ display: 'none' }} />
+                                        <AccountCircleRounded
+                                            sx={{ fontSize: 70, color: role === 'user' ? theme.palette.highlight.main : theme.palette.text.primary, marginX: 'auto' }} />
+                                    </IconButton>
+                                </div>
+                            </Grid>
+                            <Grid item xs={4} justifyContent='center'>
+                                <InputLabel
+                                    htmlFor='doctorRadio'
+                                    sx={{ whiteSpace: 'break-spaces', verticalAlign: 'top', textAlign: 'center', fontSize: '16px', marginY: '10px' }}>
+                                    <p style={{ margin: 0 }}>As</p> a doctor</InputLabel>
+                                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                    <IconButton color="highlight"
+                                        component='label' sx={{ borderBottom: role === 'doctor' ? `1px solid ${theme.palette.highlight.main}` : 'none' }}
+                                        onClick={() => {
+                                            setRole('doctor');
+                                            formik.setFieldValue('role', 'doctor');
+                                        }}>
+                                        <input type="radio" name="role" value="doctor" id="doctorRadio" style={{ display: 'none' }} />
+                                        <LocalHospitalRounded
+                                            sx={{ fontSize: 70, color: role === 'doctor' ? theme.palette.highlight.main : theme.palette.text.primary, marginX: 'auto' }} />
+                                    </IconButton>
+                                </div>
+                            </Grid>
+                        </Grid>
+                        {
+                            <Grid item xs={8} md={7} justifyContent='center' textAlign='center' marginTop='25px'>
+                                {serverResponse.msg && formSubmitted ?
+                                    <CustomAlert
+                                        severity={serverResponse.success ? 'success' : 'error'}
+                                        sx={{ boxShadow: theme.shadows[1] }}
+                                        onClose={() => {
+                                            setServerResponse({ success: false, msg: '' });
+                                            setFormSubmitted(false);
+                                        }}>{serverResponse.msg}</CustomAlert>
+                                    : formSubmitted && <CircularProgress color='highlight' sx={{ marginY: '10px' }} />}
+                            </Grid>
+                        }
+                        <Grid item xs={8} md={7} marginTop='25px'>
                             <InputField
                                 id="email"
                                 name="email"
@@ -63,7 +118,7 @@ const UserSignInForm = () => {
                                 placeholder="Enter your email"><EmailRoundedIcon />
                             </InputField>
                         </Grid>
-                        <Grid item xs={6} marginTop='25px' marginLeft='1px'>
+                        <Grid item xs={8} md={7} marginTop='25px'>
                             <InputField
                                 id="password"
                                 name="password"
@@ -73,17 +128,17 @@ const UserSignInForm = () => {
                             >
                                 <VpnKeyRoundedIcon />
                                 {isVisible ?
-                                    <VisibilityRoundedIcon onClick={handlePass} cursor={'pointer'} /> :
-                                    <VisibilityOffRoundedIcon onClick={handlePass} cursor={'pointer'} />}
+                                    <VisibilityOffRounded onClick={handlePass} cursor={'pointer'} /> :
+                                    <VisibilityRounded onClick={handlePass} cursor={'pointer'} />}
                             </InputField>
                         </Grid>
-                        <Grid container item xs={6} justifyContent={'space-between'} marginTop='25px'>
+                        <Grid container item xs={8} md={7} justifyContent={'space-between'} marginTop='25px'>
                             <FormGroup>
                                 <CustomCheckbox label="Remember me" name='rememberMe' />
                             </FormGroup>
-                            <Link href="#" style={{ ...Theme.typography.body2, alignSelf: 'center', color: Theme.palette.highlight.main }}>
+                            <Link href="#" style={{ ...theme.typography.body2, alignSelf: 'center', color: theme.palette.highlight.main }}>
                                 Forgot password?
-                                </Link>
+                            </Link>
                         </Grid>
                         <Grid container marginTop='50px' marginLeft='1px' justifyContent={'center'}>
                             <Grid item xs={4} marginBottom='50px'>
@@ -91,10 +146,10 @@ const UserSignInForm = () => {
                             </Grid>
                         </Grid>
                     </Grid>
-                </Form>
-            </Formik>
+                </Form >)}
+            </Formik >
 
-        </Grid>
+        </Grid >
     )
 }
 export default UserSignInForm;
