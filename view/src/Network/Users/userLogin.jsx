c144df36411f85a6b55dba4d62e462ee561d6c13
Basem98 <basem.mostafa98@hotmail.com>
2022-08-03 21:21:41 +0200

    feat: Include a condition that determines which endpoint to use for loggin in

--- view/src/Network/Users/userLogin.jsx
+++ view/src/Network/Users/userLogin.jsx
@@ -1,6 +1,14 @@
 import axiosClient from "../axiosClient";
 
-const submitUserData =(userData)=>{
-return axiosClient.post('/api/users/login',userData)
+const submitUserData = (userData) => {
+  let body = { ...userData };
+  let path = '/login'
+  if (body.role === 'user')
+    path = '/api/users' + path;
+  else if (body.role === 'doctor')
+    path = '/api/doctors' + path;
+
+  delete body.role;
+  return axiosClient.post(path, userData);
 }
 export default submitUserData
