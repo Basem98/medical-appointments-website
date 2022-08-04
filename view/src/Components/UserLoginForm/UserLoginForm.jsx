ecb055e643da5b3f749e9e43db2d547e876e01ea
Mohamed <mokasha810@gmail.com>
2022-08-04 09:25:04 +0200

    feat: Link SignIn with Navbar, Add global state for Logged user

--- view/src/Components/UserLoginForm/UserLoginForm.jsx
+++ view/src/Components/UserLoginForm/UserLoginForm.jsx
@@ -1,4 +1,4 @@
-import { Checkbox, FormGroup, Grid, FormControlLabel, Link, Button } from '@mui/material';
+import { Checkbox, FormGroup, Grid, FormControlLabel, Link, Button, Modal } from '@mui/material';
 import { Form, Formik } from 'formik'
 import { useTheme } from '@mui/material';
 import InputField from '../InputField/InputField';
@@ -13,10 +13,13 @@ import { useState } from 'react';
 import CustomCheckbox from '../CustomCheckbox/CustomCheckbox';
 import axios from 'axios'
 import submitUserData from '../../Network/Users/userLogin';
+import { useDispatch } from "react-redux";
+import { setUserDetails } from "../../Store/Features/UserDetails/userDetailsSlice";
 
-const User_Login_url ='/userlogin'
+const User_Login_url = '/userlogin'
 
-const UserSignInForm = () => {
+const UserSignInForm = ({ open, handleClose }) => {
+    const dispatch = useDispatch()
     const [isVisible, setVisibility] = useState(false)
     const handlePass = () => {
         if (isVisible) {
@@ -30,71 +33,78 @@ const UserSignInForm = () => {
     const initialValues = {
         email: "",
         password: "",
-        rememberMe:false
+        rememberMe: false
     };
-    const validationSchema=Yup.object({
+    const validationSchema = Yup.object({
         email: Yup.string().email('You must provide a valid email (example@organization.domain)').required('This field is required'),
         password: Yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/g, { message: 'Your password should be at least 8 characters long and it should consist of at least one uppercase letter, one lowercase letter, one number, and one symbol' }).required('This field is required')
     });
-    const onSubmit = async (values)=>{
+    const onSubmit = async (values) => {
         const email = values.email
         const password = values.password
-        await submitUserData({email,password})
-        .then((res)=>{console.log(res.data.token)})
-        .catch(err=>console.log(err))
+        await submitUserData({ email, password })
+            .then((res) => {
+                dispatch(setUserDetails({ email: email, token: res.data.token }));
+            })
+            .then(() => {
+                handleClose();
+            })
+            .catch(err => console.log(err))
     }
 
     const Theme = useTheme()
     return (
-        <Grid container style={{ background: Theme.palette.linearFormBg.main, borderRadius: 20, boxShadow: Theme.shadows[5] }}>
-            <Grid xs={12} item style={{ ...Theme.typography.h2, color: Theme.palette.text.primary, textAlign: 'center' }}>
-                <h2 style={{ textAlign: 'center' }}>Log in</h2>
-            </Grid>
-            <Formik initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}>
-                <Form>
-                    <Grid container item justifyContent={'center'}>
-                        <Grid item xs={6} marginTop='25px' marginLeft='1px' >
-                            <InputField
-                                id="email"
-                                name="email"
-                                type="text"
-                                placeholder="Enter your email"><EmailRoundedIcon />
-                            </InputField>
-                        </Grid>
-                        <Grid item xs={6} marginTop='25px' marginLeft='1px'>
-                            <InputField
-                                id="password"
-                                name="password"
-                                type={isVisible ? "text" : "password"}
-                                placeholder="Enter your password"
+        <Modal open={open} onClose={handleClose} aria-labelledby="modal-login">
+            <Grid container style={{ background: Theme.palette.linearFormBg.main, borderRadius: 20, boxShadow: Theme.shadows[5] }}>
+                <Grid xs={12} item style={{ ...Theme.typography.h2, color: Theme.palette.text.primary, textAlign: 'center' }}>
+                    <h2 style={{ textAlign: 'center' }}>Log in</h2>
+                </Grid>
+                <Formik initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={onSubmit}>
+                    <Form>
+                        <Grid container item justifyContent={'center'}>
+                            <Grid item xs={6} marginTop='25px' marginLeft='1px' >
+                                <InputField
+                                    id="email"
+                                    name="email"
+                                    type="text"
+                                    placeholder="Enter your email"><EmailRoundedIcon />
+                                </InputField>
+                            </Grid>
+                            <Grid item xs={6} marginTop='25px' marginLeft='1px'>
+                                <InputField
+                                    id="password"
+                                    name="password"
+                                    type={isVisible ? "text" : "password"}
+                                    placeholder="Enter your password"
 
-                            >
-                                <VpnKeyRoundedIcon />
-                                {isVisible ?
-                                    <VisibilityRoundedIcon onClick={handlePass} cursor={'pointer'} /> :
-                                    <VisibilityOffRoundedIcon onClick={handlePass} cursor={'pointer'} />}
-                            </InputField>
-                        </Grid>
-                        <Grid container item xs={6} justifyContent={'space-between'} marginTop='25px'>
-                            <FormGroup>
-                                <CustomCheckbox label="Remember me" name='rememberMe' />
-                            </FormGroup>
-                            <Link href="#" style={{ ...Theme.typography.body2, alignSelf: 'center', color: Theme.palette.highlight.main }}>
-                                Forgot password?
+                                >
+                                    <VpnKeyRoundedIcon />
+                                    {isVisible ?
+                                        <VisibilityRoundedIcon onClick={handlePass} cursor={'pointer'} /> :
+                                        <VisibilityOffRoundedIcon onClick={handlePass} cursor={'pointer'} />}
+                                </InputField>
+                            </Grid>
+                            <Grid container item xs={6} justifyContent={'space-between'} marginTop='25px'>
+                                <FormGroup>
+                                    <CustomCheckbox label="Remember me" name='rememberMe' />
+                                </FormGroup>
+                                <Link href="#" style={{ ...Theme.typography.body2, alignSelf: 'center', color: Theme.palette.highlight.main }}>
+                                    Forgot password?
                                 </Link>
-                        </Grid>
-                        <Grid container marginTop='50px' marginLeft='1px' justifyContent={'center'}>
-                            <Grid item xs={4} marginBottom='50px'>
-                                <CustomFormButton variant='contained' type={'submit'} fullWidth>Log in</CustomFormButton>
+                            </Grid>
+                            <Grid container marginTop='50px' marginLeft='1px' justifyContent={'center'}>
+                                <Grid item xs={4} marginBottom='50px'>
+                                    <CustomFormButton variant='contained' type={'submit'} fullWidth>Log in</CustomFormButton>
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
-                </Form>
-            </Formik>
+                    </Form>
+                </Formik>
 
-        </Grid>
+            </Grid>
+        </Modal>
     )
 }
 export default UserSignInForm;
