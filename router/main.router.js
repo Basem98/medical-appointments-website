acd6e16ea17b9f1d83ceab4533bdabfdccecc4c2
Basem98 <basem.mostafa98@hotmail.com>
2022-08-06 21:38:48 +0200

    docs: Comment the endpoints in the base route's controllers

--- router/main.router.js
+++ router/main.router.js
@@ -6,9 +6,10 @@ const adminRouter = require('./admin.router');
 const appointmentRouter = require('./appointment.router');
 const {verifyToken, logout} = require('../controller/base.controller');
 
-
+/* ---------- An endpoint to verify the user's email ---------- */
 baseRouter.put('/verify', verifyToken);
 
+/* ---------- An endpoint to logout users & delete their cookies ---------- */
 baseRouter.get('/logout', logout);
 
 module.exports = {
