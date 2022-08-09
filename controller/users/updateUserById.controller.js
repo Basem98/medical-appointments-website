b00037c9d7226bc20e940cc73083f4efe4ee2b5e
unknown <belalelmeligy516@gmail.com>
2022-08-09 04:06:52 +0200

    refactor: Remove the email handling part from update user's data and to be separated in another feature and update only firstName, lastName, and phoneNumber

--- controller/users/updateUserById.controller.js
+++ controller/users/updateUserById.controller.js
@@ -9,13 +9,20 @@ module.exports.updateUserById = async (req, res, next) => {
     if (req.params.id.toString() != req.body.id) {
         return res.status(401).json({ message: 'Unauthorized to update user data' });
     }
-    User.findByIdAndUpdate(req.body.id, { $set: req.body })
-        .then((data) => {
-            if (!data) {
-                console.log(data);
-                return res.status(404).json({ message: 'User is not found' });
-            }
-            return res.status(200).json({ message: 'User updated successfully' });
-        })
-        .catch(error => next(error))
+    try {
+        let userData = await User.findById(req.body.id);
+        if(!userData) {
+            return res.status(404).json({ message: 'User is not found' });
+        }
+        userData.firstName = req.body.firstName;
+        userData.lastName = req.body.lastName;
+        userData.phoneNumber = req.body.phoneNumber;
+
+        await userData.save();
+
+        return res.status(200).json({message: 'User updated successfully'});
+    } catch (error) {
+        error.statusCode = 500;
+        next(error);
+    }
 }
