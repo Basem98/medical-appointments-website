152a7221bddf6b7d59f0c12e19fccd46f6394327
Basem98 <basem.mostafa98@hotmail.com>
2022-08-09 16:14:31 +0200

    feat: Finalize the Feedback component

--- view/src/Components/Feedback/Feedback.jsx
+++ view/src/Components/Feedback/Feedback.jsx
@@ -2,7 +2,9 @@ import { CircularProgress, Grid, Typography, useTheme } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import TroubleshootingSvg from '../../Assets/Images/error.svg';
+import resendVerification from '../../Network/Base/resendVerification';
 import verifyToken from '../../Network/Base/verifyToken';
+import CustomAlert from '../CustomAlert/CustomAlert';
 import CustomFormButton from '../CustomFormButton/CustomFormButton';
 
 
@@ -11,9 +13,11 @@ import CustomFormButton from '../CustomFormButton/CustomFormButton';
 const Feedback = ({ msg, isConfirmed, children }) => {
   const theme = useTheme();
   const { pathname } = useLocation();
-  const { token } = useParams();
+  const { token, userId, role} = useParams();
   const [showMsg, setShowMsg] = useState(false);
   const [errMsg, setErrMsg] = useState('');
+  const [serverResponse, setServerResponse] = useState({ success: false, msg: '' });
+  const [isResendClicked, setIsResendClicked] = useState(false);
   // Add a function that calls the /verify-old endpoint to regenerate a token and bind it to the button's onClick listener
   const isConfirmation = () => {
     if (/^\/verification\//.test(pathname)) {
@@ -26,7 +30,7 @@ const Feedback = ({ msg, isConfirmed, children }) => {
         })
         .catch(err => {
           setShowMsg(true);
-          setErrMsg("You email couldn't be verified. Please click here to recieve another mail.");
+          setErrMsg("Your email couldn't be verified. Please click here to recieve another mail.");
         })
     } else {
       setErrMsg('');
@@ -35,15 +39,37 @@ const Feedback = ({ msg, isConfirmed, children }) => {
   };
   useEffect(isConfirmation, []);
 
+  const resendVerificationEmail = () => {
+    setIsResendClicked(true);
+    resendVerification(userId, role)
+      .then(res => {
+        if (res.status === 200) {
+          setIsResendClicked(false);
+          setServerResponse({ success: true, msg: 'An email has been sent to your email to verify it.' });
+        }
+      })
+      .catch(err => {
+        setIsResendClicked(false);
+        setServerResponse({ success: false, msg: 'Could not send a verification email. Please, try again later, or contact us if the problem persists.' });;
+      })
+  }
+
   return (
-    <Grid container justifyContent='space-evenly' alignItems='center' marginTop="50px" minHeight='100vh' sx={{ background: theme.palette.linearHeroBg.main }} >
+    <Grid container justifyContent='space-evenly' alignItems='center' paddingBottom='100px' marginTop="50px" minHeight='100vh' sx={{ background: theme.palette.linearHeroBg.main }} >
       {
         showMsg ?
           <>
             <Grid item xs={10} sm={8} md={6} lg={4}>
               <img alt="Page not found" src={errMsg ? TroubleshootingSvg : children.type} style={{ width: '100%' }} />
             </Grid>
-            <Grid container item xs={10} md={7} lg={5} justifyContent={{ md: 'center', lg: 'flex-start' }}>
+            <Grid container item xs={10} md={7} lg={5} justifyContent={{ xs: 'center', lg: 'flex-start' }}>
+              <Grid item xs={12}>
+                {
+                  !isResendClicked && serverResponse.success ?
+                    <CustomAlert severity='success' onClose={() => setServerResponse({ success: false, msg: '' })}>{serverResponse.msg}</CustomAlert>
+                    : serverResponse.msg && <CustomAlert severity='error' onClose={() => setServerResponse({ success: false, msg: '' })}>{serverResponse.msg}</CustomAlert>
+                }
+              </Grid>
               <Grid item xs={12}>
                 <Typography variant='h2' sx={{ color: theme.palette.text.primary, textAlign: { xs: 'center', lg: 'left' } }}>
                   {
@@ -53,13 +79,21 @@ const Feedback = ({ msg, isConfirmed, children }) => {
               </Grid>
               {
                 errMsg &&
-                <Grid item xs={3} marginTop='25px'>
-                  <CustomFormButton variant='contained' fullWidth onClick={isConfirmation}>Resend Mail</CustomFormButton>
+                <Grid container item xs={12} justifyContent={{ xs: 'center', lg: 'space-between' }}>
+                  <Grid item xs={3} marginTop='25px'>
+                    <CustomFormButton variant='contained' fullWidth onClick={resendVerificationEmail}>Resend Mail</CustomFormButton>
+                  </Grid>
+                  {
+                    isResendClicked &&
+                    <Grid marginLeft='5px' display='flex' alignItems='flex-end'>
+                      <CircularProgress color='highlight' />
+                    </Grid>
+                  }
                 </Grid>
               }
             </Grid>
           </>
-          : <CircularProgress />
+          : <CircularProgress color='highlight' />
       }
     </Grid>
   );
