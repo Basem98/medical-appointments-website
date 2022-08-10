9cc296e19850fed61edf1e113db7a648eaf55548
Basem98 <basem.mostafa98@hotmail.com>
2022-08-10 19:13:58 +0200

    feat: Implement the /api/admin/ POST endpoint to register admins

---
+++ controller/admin/main.controller.js
@@ -0,0 +1,7 @@
+const signUp = require('./signup.controller');
+const logIn = require('./login.controller');
+
+module.exports = {
+  signUp,
+  logIn
+}
--- router/admin.router.js
+++ router/admin.router.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const adminRouter = express.Router();
+const { signUp } = require('../controller/admin/main.controller');
+const validationResult = require('../middleware/user/validation.middleware');
+const validateAdminData = require('../middleware/admin/validation.middleware');
+
+
+adminRouter.post('/', validateAdminData, validationResult, signUp);
 
 
 module.exports = adminRouter;
