dcc4a6585b0a7339a82374800d841a8e2e64fa03
unknown <belalelmeligy516@gmail.com>
2022-08-11 15:43:17 +0200

    feat: Implement general auhtentication middleware to provide authentication based on role (user, doctor, or admin)

---
+++ middleware/generalAuthentication.middleware.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const envConfig = require("../config/envConfig");
+
+module.exports = (req, res, next) => {
+
+    const accessToken = req.cookies['accessToken'];
+    let role = "doctor";
+    let verifiedToken;
+    try {
+        switch (role) {
+            case 'user':
+                verifiedToken = jwt.verify(accessToken, envConfig.AUTH.USER_SECRET);
+                break;
+            case 'doctor':
+                verifiedToken = jwt.verify(accessToken, envConfig.AUTH.DOCTOR_SECRET);
+                break;
+            case 'admin':
+                verifiedToken = jwt.verify(accessToken, envConfig.AUTH.ADMIN_SECRET);
+                break;
+            default:
+                throw new Error("Role is not found");
+        }
+    } catch (error) {
+        error.statusCode = 404;
+        next(error);
+
+    }
+    finally {
+        if(verifiedToken) {
+            console.log(verifiedToken);
+            req.body.id = verifiedToken.id
+        }
+        next();
+    }
+}
