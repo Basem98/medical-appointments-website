b6df1e3f5e9a6f2b4ea7016813081d2925878d1f
unknown <belalelmeligy516@gmail.com>
2022-08-11 09:23:50 +0200

    refactor: Cancel sending the token in the headers and rely on cookies for authentication

--- view/src/Network/Users/changePassword.jsx
+++ view/src/Network/Users/changePassword.jsx
@@ -1,12 +1,8 @@
 import axiosClient from "../axiosClient";
 
 const changePassword = (id, token, data) => {
-    const config = {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    }
-    return axiosClient.patch(`/users/${id}/change-password`, data, config);
+
+    return axiosClient.patch(`/users/${id}/change-password`, data);
 }
 
 export default changePassword;
