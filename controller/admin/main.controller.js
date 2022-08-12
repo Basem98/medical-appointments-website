347d2e2018209208692f0813818816624c6bd115
Basem98 <basem.mostafa98@hotmail.com>
2022-08-12 16:51:31 +0200

    feat: Implement the /api/admins/doctors/ GET endpoint

--- controller/admin/main.controller.js
+++ controller/admin/main.controller.js
@@ -4,12 +4,13 @@ const getDoctorApplications = require('./applications.controller');
 const getLogs = require('./logs.controller');
 const acceptDoctorsApplication = require('./accept.controller');
 const deleteDoctor = require('../admin/deleteDoctor.controller');
-
+const getAllDoctors = require('../admin/doctors.controller');
 module.exports = {
   signUp,
   logIn,
   getDoctorApplications,
   getLogs,
   acceptDoctorsApplication,
-  deleteDoctor
+  deleteDoctor,
+  getAllDoctors
 }
--- router/admin.router.js
+++ router/admin.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const adminRouter = express.Router();
-const { signUp, logIn, getDoctorApplications, getLogs, acceptDoctorsApplication, deleteDoctor } = require('../controller/admin/main.controller');
+const { signUp, logIn, getDoctorApplications, getLogs, acceptDoctorsApplication, deleteDoctor, getAllDoctors } = require('../controller/admin/main.controller');
 const validationResult = require('../middleware/user/validation.middleware');
 const validateAdminData = require('../middleware/admin/validation.middleware');
 const protectAdminsRoute = require('../middleware/admin/auth.middleware');
@@ -12,16 +12,18 @@ adminRouter.post('/', validateAdminData, validationResult, signUp);
 /* ---------- An endpoint to log as admin in ---------- */
 adminRouter.post('/login', validateAdminData.slice(1), validationResult, logIn);
 
+adminRouter.get('/doctors/all', protectAdminsRoute, getAllDoctors);
+
 /* ---------- An endpoint to get all non-accepted doctors' applications ---------- */
 adminRouter.get('/doctors', protectAdminsRoute, getDoctorApplications);
-
-/* ---------- An endpoint to get all logs by page ---------- */
-adminRouter.get('/logs', protectAdminsRoute, getLogs);
 
 /* ---------- An endpoint to accept a doctor's application ---------- */
 adminRouter.put('/doctors/accept/:id', protectAdminsRoute, acceptDoctorsApplication);
 
 /* ---------- An endpoint to accept a doctor's application ---------- */
 adminRouter.delete('/doctors/delete/:id', protectAdminsRoute, deleteDoctor);
+
+/* ---------- An endpoint to get all logs by page ---------- */
+adminRouter.get('/logs', protectAdminsRoute, getLogs);
 
 module.exports = adminRouter;
