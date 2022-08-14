d2cc61b3dc58431d91d933f8b18b233ca834f94e
Basem98 <basem.mostafa98@hotmail.com>
2022-08-14 12:19:00 +0200

    refactor: Add pagination to the /api/admins/appointments/upcoming GET endpoint

--- controller/admin/upcomingAppointment.controller.js
+++ controller/admin/upcomingAppointment.controller.js
@@ -3,7 +3,13 @@ const Appointments = require('../../model/appointment.model');
 
 const getUpcomingAppointments = async (req, res, next) => {
   try {
-    const upcomingAppointments = await Appointments.find({ date: { $gte: new Date() } });
+    const page = parseInt(req.query.page) || 0;
+    const limit = parseInt(req.query.limit) || 0;
+    const upcomingAppointments = await Appointments
+      .find({ date: { $gte: new Date() } })
+      .sort({ date: 1 })
+      .skip(page * limit)
+      .limit(limit);
     if (upcomingAppointments.length < 1) {
       const err = new Error('No upcoming appointments found');
       err.statusCode = 404;
