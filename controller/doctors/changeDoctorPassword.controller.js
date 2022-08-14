539845caa8be2078d9d381c713ea738945ab03e0
unknown <belalelmeligy516@gmail.com>
2022-08-14 12:33:20 +0200

    feat: Implement patch endpoint to change doctor's password

---
+++ controller/doctors/changeDoctorPassword.controller.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Docotor = require("../../model/doctor.model");
+const bcrypt = require("bcrypt");
+
+const changeDoctorPassword = async (req, res, next) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(404).json({ message: 'Doctor is Not Found' });
+    }
+    if (req.params.id.toString() != req.body.id) {
+        return res.status(401).json({ message: 'Unauthorized to change password' });
+    }
+    try {
+        let doctorPassword = await Docotor.findById(req.body.id, { password: 1 });
+        bcrypt.compare(req.body.currentPassword, doctorPassword.password)
+            .then(async (isCorrectPassword) => {
+                if (!isCorrectPassword) {
+                    return res.status(401).json({ message: 'Wrong password' });
+                }
+                doctorPassword.password = req.body.password;
+                await doctorPassword.save();
+                return res.status(200).json({ message: 'Password changed successfully!' });
+            })
+            .catch((error) => {
+                error.statusCode = 500;
+                next(error)
+            })
+
+    } catch (error) {
+        error.statusCode = 500;
+        next(error);
+    }
+}
+
+module.exports = changeDoctorPassword;
