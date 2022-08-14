d471212f656aa2f7d5eef21a9976d19e01b2367f
Mohamed <mokasha810@gmail.com>
2022-08-14 16:44:18 +0200

    feat: Handle Admin special login

--- view/src/Network/Base/login.jsx
+++ view/src/Network/Base/login.jsx
@@ -7,8 +7,9 @@ const submitUserData = (userData) => {
     path = '/users' + path;
   else if (body.role === 'Doctor')
     path = '/doctors' + path;
-
+  else if (body.role === 'Admin')
+    path = '/admins/' + path;
   delete body.role;
-  return axiosClient.post(path, userData);
+  return axiosClient.post(path, body);
 }
 export default submitUserData
