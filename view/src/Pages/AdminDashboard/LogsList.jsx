29615e94631848b5c0024894d024b5c85051a197
Mohamed <mokasha810@gmail.com>
2022-08-14 18:11:12 +0200

    feat: Add pagination to logs section

--- view/src/Pages/AdminDashboard/LogsList.jsx
+++ view/src/Pages/AdminDashboard/LogsList.jsx
@@ -1,6 +1,5 @@
 import getAdminLogs from '../../Network/Admin/logs'
 import { useEffect, useState } from 'react';
-import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
@@ -8,19 +7,29 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { StyledTableCell, StyledTableRow } from '../../Helper/CustomTableItems'
-import { Grid } from '@mui/material';
-
-
+import { Grid, Box } from '@mui/material';
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
 
 const LogsList = () => {
 
     const [logsData, setLogsData] = useState([]);
+    const [paginatePageNumber, setPaginatePageNumber] = useState(0);
 
     useEffect(() => {
-        getAdminLogs()
+        getAdminLogs(paginatePageNumber)
             .then(res => setLogsData(res.data.data))
             .catch(err => console.log(err))
-    }, [])
+    }, [paginatePageNumber])
+
+    const handlePageForward = () => {
+        console.log('paginatePageNumber: ', paginatePageNumber)
+        setPaginatePageNumber(paginatePageNumber + 1);
+    }
+
+    const handlePageBackward = () => {
+        if (paginatePageNumber > 0) setPaginatePageNumber(paginatePageNumber - 1);
+    }
 
     return (
         <Grid item xs={11} sx={{ py: 3 }}>
@@ -38,7 +47,6 @@ const LogsList = () => {
                             <StyledTableCell align="left">Status Code</StyledTableCell>
                             <StyledTableCell align="left">Message</StyledTableCell>
 
-
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -52,6 +60,10 @@ const LogsList = () => {
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Box sx={{ mt: 3, px: 15, display: "flex", justifyContent: "space-between" }}>
+                <ArrowCircleLeftOutlinedIcon onClick={handlePageBackward} sx={{ fontSize: 40 }} />
+                <ArrowCircleRightOutlinedIcon onClick={handlePageForward} sx={{ fontSize: 40 }} />
+            </Box>
         </Grid>
 
     )
