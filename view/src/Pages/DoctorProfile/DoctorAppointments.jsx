ae32896322a5f06b8eccfe440afe857f7430e09e
unknown <belalelmeligy516@gmail.com>
2022-08-14 10:11:56 +0200

    feat: Implement displayinh doctor's previous appointment in appointments page

--- view/src/Pages/DoctorProfile/DoctorAppointments.jsx
+++ view/src/Pages/DoctorProfile/DoctorAppointments.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import ContentToggler from "../../Components/ContentToggler/ContentToggler";
 import getUpcomings from "../../Network/Doctors/getUpcomings";
+import getPrevious from "../../Network/Doctors/getPrevious";
 
 const Appointments = () => {
     const titles = ["Upcoming Appointments", "Previous Appointments"];
@@ -19,6 +20,17 @@ const Appointments = () => {
             .then((response) => {
                 console.log(response);
                 setUpcomingAppointments(response.data.message);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, []);
+
+    useEffect(() => {
+        getPrevious("62e7c235150862d88def8161")
+            .then((response) => {
+                console.log(response);
+                setPreviousAppointments(response.data.message);
             })
             .catch((error) => {
                 console.log(error);
