6030c80e9e2ba7e84abbeedc8bd04d3e91be9cd7
unknown <belalelmeligy516@gmail.com>
2022-08-14 10:01:06 +0200

    feat: Handle fetching and displaying upcoming doctor's appointments

--- view/src/Pages/DoctorProfile/DoctorAppointments.jsx
+++ view/src/Pages/DoctorProfile/DoctorAppointments.jsx
@@ -1,16 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import ContentToggler from "../../Components/ContentToggler/ContentToggler";
-import getUpcomings from "../../Network/Users/getUpcomings";
+import getUpcomings from "../../Network/Doctors/getUpcomings";
 
 const Appointments = () => {
+    const titles = ["Upcoming Appointments", "Previous Appointments"];
+
     const [upcomingAppointments, setUpcomingAppointments] = useState(null);
     const [previousAppointments, setPreviousAppointments] = useState(null);
+
+    const data = [
+        upcomingAppointments,
+        previousAppointments
+    ];
 
     useEffect(() => {
         getUpcomings("62e7c235150862d88def8161")
             .then((response) => {
                 console.log(response);
+                setUpcomingAppointments(response.data.message);
             })
             .catch((error) => {
                 console.log(error);
@@ -18,9 +26,13 @@ const Appointments = () => {
     }, []);
 
     return (
-        <ContentToggler
-            
-        />
+        <>
+            <ContentToggler
+                titles={titles}
+                data={data}
+                role="doctor"
+            />
+        </>
     );
 }
 
