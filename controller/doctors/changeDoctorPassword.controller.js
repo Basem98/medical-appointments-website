b9c4baf5fc81d8358d95e6b6bb27715f8b572c83
unknown <belalelmeligy516@gmail.com>
2022-08-15 09:59:37 +0200

    feat: Extract doctor's id from req.doctor._id

--- controller/doctors/changeDoctorPassword.controller.js
+++ controller/doctors/changeDoctorPassword.controller.js
@@ -3,14 +3,15 @@ const Docotor = require("../../model/doctor.model");
 const bcrypt = require("bcrypt");
 
 const changeDoctorPassword = async (req, res, next) => {
+    const doctorId = req.doctor._id;
     if (!mongoose.isValidObjectId(req.params.id)) {
         return res.status(404).json({ message: 'Doctor is Not Found' });
     }
-    if (req.params.id.toString() != req.body.id) {
+    if (req.params.id.toString() != doctorId) {
         return res.status(401).json({ message: 'Unauthorized to change password' });
     }
     try {
-        let doctorPassword = await Docotor.findById(req.body.id, { password: 1 });
+        let doctorPassword = await Docotor.findById(doctorId, { password: 1 });
         bcrypt.compare(req.body.currentPassword, doctorPassword.password)
             .then(async (isCorrectPassword) => {
                 if (!isCorrectPassword) {
