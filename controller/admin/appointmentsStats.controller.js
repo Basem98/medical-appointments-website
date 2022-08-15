26ac352a3c5b8e2c72f9a6dce770150e40df1d03
Basem98 <basem.mostafa98@hotmail.com>
2022-08-15 12:46:53 +0200

    feat: Filter statistics by a date range

--- controller/admin/appointmentsStats.controller.js
+++ controller/admin/appointmentsStats.controller.js
@@ -3,12 +3,20 @@ const Appointments = require('../../model/appointment.model');
 
 const getAppointmentStatistics = async (req, res, next) => {
   try {
+    let dateFrom = req.query.dateFrom;
+    let dateTo = req.query.dateTo;
+    dateFrom = isNaN(new Date(dateFrom)) ? new Date(1970, 0, 01) : new Date(dateFrom);
+    dateTo = isNaN(new Date(dateTo)) ? new Date(3000, 0, 01) : new Date(dateTo);
     const statistics = {
-      numberOfAppointments: await Appointments.find().count(),
-      numberOfAvailableAppointments: await Appointments.find({ state: 'available' }).count(),
-      numberOfBookedAppointments: await Appointments.find({ state: 'booked' }).count(),
-      numberOfFinishedAppointments: await Appointments.find({ state: 'finished' }).count(),
-      numberOfCanceledAppointments: await Appointments.find({ state: 'canceled' }).count()
+      numberOfAppointments: await Appointments.find({ date: { $gte: dateFrom, $lt: dateTo } }).count(),
+      numberOfAvailableAppointments: await Appointments.find(
+        { state: 'available', date: { $gte: dateFrom, $lt: dateTo } }).count(),
+      numberOfBookedAppointments: await Appointments.find(
+        { state: 'booked', date: { $gte: dateFrom, $lt: dateTo } }).count(),
+      numberOfFinishedAppointments: await Appointments.find(
+        { state: 'finished', date: { $gte: dateFrom, $lt: dateTo } }).count(),
+      numberOfCanceledAppointments: await Appointments.find(
+        { state: 'canceled', date: { $gte: dateFrom, $lt: dateTo } }).count()
     }
     res.status(200).json({ data: statistics });
   } catch (err) {
