97c1e9a7ddc0eba88c1f4c691e03330478d2fb20
Mohamed <mokasha810@gmail.com>
2022-08-16 10:30:14 +0200

    feat: Add feedback notifications to logs section & enhance pagination

--- view/src/Pages/AdminDashboard/LogsList.jsx
+++ view/src/Pages/AdminDashboard/LogsList.jsx
@@ -7,32 +7,46 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { StyledTableCell, StyledTableRow } from '../../Helper/CustomTableItems'
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Snackbar } from '@mui/material';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import CustomAlert from "../../Components/CustomAlert/CustomAlert";
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 const LogsList = () => {
 
     const [logsData, setLogsData] = useState([]);
-    const [paginatePageNumber, setPaginatePageNumber] = useState(0);
+    const [paginatePage, setPaginatePage] = useState({ pageNum: 0, nextPage: true });
+    const [snakbarStatus, setSnakbarStatus] = useState(false);
+    const [alertStatus, setAlertStatus] = useState({ severity: '', msg: '' });
 
     useEffect(() => {
-        getAdminLogs(paginatePageNumber)
-            .then(res => setLogsData(res.data.data))
-            .catch(err => console.log(err))
-    }, [paginatePageNumber])
+        getAdminLogs(paginatePage.pageNum)
+            .then(res => { setLogsData(res.data.data); console.log(res) })
+            .catch(err => {
+                console.log('erro: ', err)
+                if (err.response.status === 404) {
+                    setPaginatePage({ pageNum: paginatePage.pageNum - 1, nextPage: false });
+                    setAlertStatus({ severity: 'error', msg: 'No pages found!' });
+                    setSnakbarStatus(true);
+                }
+            })
+    }, [paginatePage.pageNum])
 
     const handlePageForward = () => {
-        console.log('paginatePageNumber: ', paginatePageNumber)
-        setPaginatePageNumber(paginatePageNumber + 1);
+        console.log('paginatePage: ', paginatePage)
+        setPaginatePage({ ...paginatePage, pageNum: paginatePage.pageNum + 1 });
     }
 
     const handlePageBackward = () => {
-        if (paginatePageNumber > 0) setPaginatePageNumber(paginatePageNumber - 1);
+        if (paginatePage.pageNum > 0)
+            setPaginatePage({ ...paginatePage, pageNum: paginatePage.pageNum - 1 });
     }
 
     return (
-        <Grid item xs={11} sx={{ py: 3 }}>
+        <Grid item xs={11} sx={{ py: 3, display: "flex", flexDirection: "column", justifyContent: "space-between" }} >
             {/* <h1>Logs</h1> */}
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 700 }} aria-label="customized table">
@@ -51,7 +65,7 @@ const LogsList = () => {
                     </TableHead>
                     <TableBody>
                         {logsData.length && logsData.map((row) => (
-                            <StyledTableRow key={row.name}>
+                            <StyledTableRow key={row._id}>
                                 {Object.keys(row).map(key => (
                                     <StyledTableCell align="left">{row[key]}</StyledTableCell>
                                 ))}
@@ -60,10 +74,29 @@ const LogsList = () => {
                     </TableBody>
                 </Table>
             </TableContainer>
-            <Box sx={{ mt: 3, px: 15, display: "flex", justifyContent: "space-between" }}>
-                <ArrowCircleLeftOutlinedIcon onClick={handlePageBackward} sx={{ fontSize: 40 }} />
-                <ArrowCircleRightOutlinedIcon onClick={handlePageForward} sx={{ fontSize: 40 }} />
+            <Box sx={{ my: 3, px: 15, display: "flex", justifyContent: paginatePage.pageNum ? "space-between" : "end", position: "static", bottom: "20px" }}>
+                {paginatePage.pageNum ? (<ArrowCircleLeftOutlinedIcon cursor="pointer" onClick={handlePageBackward} sx={{ fontSize: 40, "&:hover": { opacity: .7 } }} />) : ('')}
+                {paginatePage.nextPage ? (<ArrowCircleRightOutlinedIcon cursor="pointer" onClick={handlePageForward} sx={{ fontSize: 40, "&:hover": { opacity: .7 } }} />) : ('')}
             </Box>
+            <Snackbar open={snakbarStatus} autoHideDuration={5000} onClose={() => setSnakbarStatus(false)}>
+                <CustomAlert
+                    severity={alertStatus.severity}
+                    action={
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            size="small"
+                            onClick={() => {
+                                setSnakbarStatus(false);
+                            }}
+                        >
+                            <CloseIcon fontSize="inherit" />
+                        </IconButton>
+                    }
+                >
+                    {alertStatus.msg}
+                </CustomAlert>
+            </Snackbar>
         </Grid>
 
     )
