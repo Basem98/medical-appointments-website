ab5864e4212c02095acdb54aa7f990373ba8480b
unknown <belalelmeligy516@gmail.com>
2022-08-16 08:11:18 +0200

    feat: Implement getPatients controller function

---
+++ controller/doctors/getPatients.controller.js
@@ -0,0 +1,51 @@
+const Doctor = require('../../model/doctor.model');
+const mongoose = require("mongoose");
+
+const getPatients = async (req, res, next) => {
+    const doctorId = req.doctor._id;
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(404).json({ message: 'Doctor is Not Found' });
+    }
+    if (req.params.id.toString() != doctorId) {
+        return res.status(401).json({ message: 'Unauthorized to get patients' });
+    }
+    try {
+        console.log(req.params.id)
+        const appointments = await Doctor.aggregate([
+            {
+                $match: { _id: doctorId }
+            },
+            {
+                $lookup: {
+                    from: 'appointments',
+                    localField: 'appointments._id',
+                    foreignField: '_id',
+                    as: 'appointments'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    localField:'appointments.user',
+                    foreignField: '_id',
+                    as: 'users'
+                }
+            },
+            {
+                $project: {
+                    users: 1
+                }
+            }
+        ]);
+        if (!appointments) {
+            throw new Error('Doctor is not found');
+        }
+        return res.status(200).json({ message: appointments });
+    } catch (error) {
+        error.statusCode = 500;
+        next(error);
+    }
+
+}
+
+module.exports = getPatients;
