87a9cf3accd226cfda0e1613a44d6c84bedaef8b
unknown <belalelmeligy516@gmail.com>
2022-08-17 14:08:09 +0200

    feat: Implement condotional rendering to render prescription and diagnosis based on existence and role

---
+++ view/src/Components/AppointmentDetails/AppointmentExamination.jsx
@@ -0,0 +1,152 @@
+import {
+    TableContainer,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    Grid,
+    Typography,
+    useTheme
+} from "@mui/material";
+import MedicationIcon from '@mui/icons-material/Medication';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import CustomFormButton from "../CustomFormButton/CustomFormButton";
+
+const AppointmentExamination = ({ appointmentDetails, role }) => {
+    const theme = useTheme();
+    const AppointmentDetail = (props) => {
+        return (
+
+            <Grid
+                container item
+                xs={10}
+            >
+                <Grid
+                    item
+                    xs={1}
+                >
+                    {props.children}
+                </Grid>
+                <Grid
+                    item
+                    xs={11}
+                >
+                    <Typography
+                        variant="body2"
+                        paddingLeft={2}
+                    >
+                        {props.detail}
+                    </Typography>
+                </Grid>
+            </Grid>
+
+        )
+    }
+    return (
+        <>
+            {
+                appointmentDetails?.info?.diagnosis ?
+                    <AppointmentDetail
+                        detail={appointmentDetails?.info.diagnosis}
+                    >
+                        <MonitorHeartIcon
+                            fontSize="medium"
+                            sx={{
+                                color: theme.palette.highlight.main,
+                                marginRight: 3
+                            }}
+                        />
+                    </AppointmentDetail>
+                    : role === 'doctor' ?
+                        <CustomFormButton>
+                            Add Diagnosis
+                        </CustomFormButton>
+                        :
+                        <></>
+
+
+            }
+
+            {
+                appointmentDetails?.info?.prescription?.length > 0 ?
+                <Grid
+                    container
+                    item
+                    xs={10}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        flexWrap: 'wrap'
+                    }}
+                >
+                    <Grid item xs={1}>
+                        <MedicationIcon
+                            fontSize="medium"
+                            sx={{
+                                color: theme.palette.highlight.main,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item xs={1}>
+                        <Typography
+                            variant="body2"
+                            paddingLeft={2}
+                        >
+                            Prescription
+                        </Typography>
+                    </Grid>
+                    <TableContainer>
+                        <Table sx={{ maxWidth: 400 }} aria-label="Prescription">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell sx={{
+                                        color: theme.palette.highlight.main,
+                                        fontWeight: 'bold'
+                                    }}
+                                    >
+                                        Drug Name</TableCell>
+                                    <TableCell sx={{
+                                        color: theme.palette.highlight.main,
+                                        fontWeight: 'bold'
+                                    }}
+                                    >
+                                        Dosage
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+
+                                    appointmentDetails?.info?.prescription.map((drug) => {
+                                        return (
+                                            <TableRow
+                                                key={drug.drugName}
+                                            >
+                                                <TableCell component="th">
+                                                    {drug.drugName}
+                                                </TableCell>
+                                                <TableCell>
+                                                    {drug.dosage}
+                                                </TableCell>
+                                            </TableRow>
+                                        );
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                : role === 'doctor' ? 
+                <CustomFormButton>
+                    Add Prescription
+                </CustomFormButton>
+                :
+                <></>
+            }
+
+        </>
+    );
+}
+
+export default AppointmentExamination;
