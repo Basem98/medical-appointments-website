1fa47fe11b94e12684c0e19e83be57c02caf445d
Mohamed <mokasha810@gmail.com>
2022-08-17 11:01:38 +0200

    feat: Add filters to pie charts & handle api calls

--- view/src/Network/Admin/statistics.jsx
+++ view/src/Network/Admin/statistics.jsx
@@ -1,11 +1,12 @@
 import axiosClient from "../axiosClient";
+import { parseDate } from '../../Helper/ParseDate';
 
 export const getAppointments = (searchDates) => {
     let path = '/admins/appointments/statistics';
     let queryData = {};
     if (searchDates) {
-        queryData.dateFrom = `${new Date().getFullYear()}-${searchDates.dateFromMonth}-${searchDates.dateFromDay}`;
-        queryData.dateTo = `${new Date().getFullYear()}-${searchDates.dateToMonth}-${searchDates.dateToDay}`;
+        queryData = parseDate(searchDates);
+        console.log('queryData: ', queryData)
     }
     return axiosClient.get(path, {
         params: {
@@ -18,10 +19,9 @@ export const getSignups = (searchDates) => {
     let path = '/admins/users/statistics';
     let queryData = {};
     if (searchDates) {
-        queryData.dateFrom = `${new Date().getFullYear()}-${searchDates.dateFromMonth}-${searchDates.dateFromDay}`;
-        queryData.dateTo = `${new Date().getFullYear()}-${searchDates.dateToMonth}-${searchDates.dateToDay}`;
+        queryData = parseDate(searchDates);
+        console.log('queryData: ', queryData)
     }
-
     return axiosClient.get(path, {
         params: {
             ...queryData
