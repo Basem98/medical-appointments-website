c4255c98585e1a4dcb9e41924ec86bd18e8a5eec
unknown <belalelmeligy516@gmail.com>
2022-08-17 11:43:26 +0200

    fix: Add doctor id to the useEffect dependencies array

--- view/src/Pages/DoctorProfile/DoctorAppointments.jsx
+++ view/src/Pages/DoctorProfile/DoctorAppointments.jsx
@@ -1,22 +1,47 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import { useState } from "react";
 import ContentToggler from "../../Components/ContentToggler/ContentToggler";
 import getUpcomings from "../../Network/Doctors/getUpcomings";
 import getPrevious from "../../Network/Doctors/getPrevious";
+import checkAuthentication from "../../Network/Base/checkAuthentication";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserDetails } from "../../Store/Features/UserDetails/userDetailsSlice";
+import { useNavigate } from "react-router-dom";
 
 const Appointments = () => {
     const titles = ["Upcoming Appointments", "Previous Appointments"];
-
+    const doctorId = useSelector((state) => state.userDetails.data?._id);
     const [upcomingAppointments, setUpcomingAppointments] = useState(null);
     const [previousAppointments, setPreviousAppointments] = useState(null);
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
     const data = [
         upcomingAppointments,
         previousAppointments
     ];
 
     useEffect(() => {
-        getUpcomings("62f8f6acc5842627fdd9d631")
+        checkAuthentication()
+            .then((response) => {
+                dispatch(setUserDetails({
+                    role: response.data.role,
+                    data: response.data.data,
+                    email: response.data.data.email
+                }))
+                if (response.data.role !== 'Doctor') {
+                    navigate('/');
+                }
+            })
+            .catch((error) => {
+                navigate('/');
+            })
+    }, []);
+
+    useEffect(() => {
+        getUpcomings(doctorId)
             .then((response) => {
                 console.log(response);
                 setUpcomingAppointments(response.data.message);
@@ -24,10 +49,10 @@ const Appointments = () => {
             .catch((error) => {
                 console.log(error);
             })
-    }, []);
+    }, [doctorId]);
 
     useEffect(() => {
-        getPrevious("62f8f6acc5842627fdd9d631")
+        getPrevious(doctorId)
             .then((response) => {
                 console.log(response);
                 setPreviousAppointments(response.data.message);
@@ -35,7 +60,7 @@ const Appointments = () => {
             .catch((error) => {
                 console.log(error);
             })
-    }, []);
+    }, [doctorId]);
 
     return (
         <>
