2997be6fa4c7a9bce5d6cdb8702aec8e0acc7cf7
Basem98 <basem.mostafa98@hotmail.com>
2022-08-18 15:48:58 +0200

    fix: Make the DoctorCard's size consistent

--- view/src/Components/DoctorCard/DoctorCard.jsx
+++ view/src/Components/DoctorCard/DoctorCard.jsx
@@ -5,24 +5,26 @@ import {
   Container,
   CircularProgress,
 } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import CustomFormButton from "../CustomFormButton/CustomFormButton";
 
 const DoctorCard = ({ cardData }) => {
   const theme = useTheme();
-  const navigate = useNavigate();
+
   return (
     <Container
       disableGutters
       sx={{
         backgroundColor: "white",
-        // maxWidth: "300px",
+        width: "350px",
+        height:"650px",
         padding: "30px 20px",
         borderRadius: "10px",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         boxShadow: "0px 4px 4px rgba(0, 0, 0, .25)",
+        position: 'relative'
       }}
     >
       {cardData ? (
@@ -30,7 +32,7 @@ const DoctorCard = ({ cardData }) => {
           <img
             src={cardData.profilePicture}
             alt="doctorImage"
-            style={{ borderRadius: "50%", maxWidth: "80%" }}
+            style={{ borderRadius: "50%", width: '250px', height: '250px', objectFit: 'cover', boxShadow: theme.shadows[5]}}
           />
           <Container
             disableGutters
@@ -40,7 +42,7 @@ const DoctorCard = ({ cardData }) => {
               justifyContent: "center",
             }}
           >
-            <Rating defaultValue={cardData.rating} precision={.5} size="large" readOnly />
+            <Rating value={parseFloat(cardData.rating)} precision={.5} size="large" readOnly />
           </Container>
           <Typography variant="h4" align="center">
             Dr. {cardData.firstName + ' ' + cardData.lastName}
@@ -48,15 +50,25 @@ const DoctorCard = ({ cardData }) => {
           <Typography variant="h6" align="center" sx={{ my: 2, p: 0 }}>
             Specialized in {cardData.specialization}
           </Typography>
+          <Typography variant="body2" align="center" sx={{ my: 2, p: 0 }}>
+            Appointment Fees: {cardData.clinics[0].fees}
+          </Typography>
           <Link
             to={'/specialists/details'}
             state={{ doctorData: cardData }}
+            style={{
+              textDecoration: 'none'
+            }}
           >
             <CustomFormButton
               variant="contained"
               sx={{
                 padding: "10px 30px",
                 fontSize: theme.typography.body1.fontSize,
+                position: 'absolute',
+                bottom: 20,
+                left: '50%',
+                transform: 'translateX(-50%)'
               }}
             >
               More Info
