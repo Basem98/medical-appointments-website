6172615f95ffa206e39cf8e6034eee4b7bfddb50
unknown <belalelmeligy516@gmail.com>
2022-08-18 13:49:12 +0200

    feat: Implement conditional rendering to render the cancellation button or the confirm cancellation component

--- view/src/Components/AppointmentDetails/AppointmentCancellation.jsx
+++ view/src/Components/AppointmentDetails/AppointmentCancellation.jsx
@@ -1,24 +1,38 @@
 import { Grid, Button, useTheme } from "@mui/material";
+import { useState } from "react";
+import AppointmentCancellationConfirm from "./AppointmentCancellationConfirm";
 
 const AppointmentCancellation = ({ role, state }) => {
     const theme = useTheme();
-    return role === 'user' && state === 'booked' ? 
-    (
-        <Grid item xs={12}>
-            <Button
-                sx={{
-                    backgroundColor: theme.palette.error.main,
-                    color: 'white',
-                    '&:hover': {
-                        border: `1px solid ${theme.palette.error.main}`,
-                        color: theme.palette.error.main
-                    }
-                }}
-            >
-                Cancel Appointment
-            </Button>
-        </Grid>
-    ) : <></>
+    const [wantToCancel, setWantToCancel] = useState(false);
+    const handleCancellation = () => {
+        setWantToCancel(!wantToCancel);
+    }
+    return role === 'user' && state === 'booked' ?
+        (
+            <Grid item xs={12}>
+                {
+                    wantToCancel ?
+                        <AppointmentCancellationConfirm
+                            handleCancellation={handleCancellation}
+                        />
+                        :
+                        <Button
+                            sx={{
+                                backgroundColor: theme.palette.error.main,
+                                color: 'white',
+                                border: `1px solid ${theme.palette.error.main}`,
+                                '&:hover': {
+                                    color: theme.palette.error.main
+                                }
+                            }}
+                            onClick={handleCancellation}
+                        >
+                            Cancel Appointment
+                        </Button>
+                }
+            </Grid>
+        ) : <></>
 }
 
 export default AppointmentCancellation;
