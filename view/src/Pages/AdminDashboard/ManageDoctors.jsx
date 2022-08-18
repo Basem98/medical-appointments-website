78901ecf1f53035a7c104e1c749834d188631d00
Mohamed <mokasha810@gmail.com>
2022-08-18 17:28:20 +0200

    feat: Implement Review Doctor Application UI

--- view/src/Pages/AdminDashboard/ManageDoctors.jsx
+++ view/src/Pages/AdminDashboard/ManageDoctors.jsx
@@ -10,6 +10,7 @@ import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutl
 import CustomAlert from "../../Components/CustomAlert/CustomAlert";
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import ReviewDoctorApplication from './ReviewDoctorApplication';
 
 
 export default function ManageDoctors() {
@@ -17,6 +18,8 @@ export default function ManageDoctors() {
     const [paginatePage, setPaginatePage] = useState({ pageNum: 0, nextPage: true });
     const [snakbarStatus, setSnakbarStatus] = useState(false);
     const [alertStatus, setAlertStatus] = useState({ severity: '', msg: '' });
+    const [open, setOpen] = useState(false);
+    const [currentDoctorRow, setCurrentDoctorRow] = useState({});
 
     useEffect(() => {
         getDoctorsData({ type: '', pageNum: paginatePage.pageNum })
@@ -43,84 +46,85 @@ export default function ManageDoctors() {
         if (paginatePage.pageNum > 0) setPaginatePage({ nextPage: true, pageNum: paginatePage.pageNum - 1 });
     }
 
-    const handleAcceptDoctor = () => {
+    const handleDrawerOpen = (doctorRow) => {
+        setOpen(true);
+        console.log('handleDrawerOpen: ', doctorRow);
+        setCurrentDoctorRow(doctorRow);
+    };
 
-    }
-    const handleRejectDoctor = (userId) => {
-        acceptDoctor(userId)
-            .then(res => {
-                if (res.status === 204) {
-                    setDoctorsData(doctorsData.filter(user => user._id !== userId));
-                    setAlertStatus({ severity: 'success', msg: 'User Deleted' });
-                    setSnakbarStatus(true);
-                }
-            })
-            .catch(err => {
-                setAlertStatus({ severity: 'error', msg: 'Could not delete!' });
-                setSnakbarStatus(true);
-            })
-    }
+    const handleDrawerClose = (doctorRow) => {
+        setOpen(false);
+    };
 
     return (
-        <Grid item xs={11} sx={{ py: 3, display: "flex", flexDirection: "column", justifyContent: "space-between" }} >
-            {doctorsData.length ? (
-                <>
-                    <TableContainer component={Paper}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <StyledTableCell>Name</StyledTableCell>
-                                    <StyledTableCell>E-mail</StyledTableCell>
-                                    <StyledTableCell>Phone No.</StyledTableCell>
-                                    <StyledTableCell>Education</StyledTableCell>
-                                    {/* <StyledTableCell>Verified</StyledTableCell> */}
-                                    <StyledTableCell>Details</StyledTableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {doctorsData.length && doctorsData.map(userRow => (
-                                    <StyledTableRow key={userRow._id}>
-                                        <StyledTableCell>{`${userRow.firstName} ${userRow.lastName}`}</StyledTableCell>
-                                        <StyledTableCell>{userRow.email}</StyledTableCell>
-                                        <StyledTableCell>{userRow.phoneNumber}</StyledTableCell>
-                                        {/* <StyledTableCell>{userRow.isVerified ? "Yes" : "No"}</StyledTableCell> */}
-                                        <StyledTableCell>{userRow.education[0].degree}</StyledTableCell>
-                                        <StyledTableCell><Button variant="contained" onClick={() => handleAcceptDoctor(userRow._id)}>More details</Button></StyledTableCell>
-                                    </StyledTableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <Box sx={{ my: 3, px: 15, display: "flex", justifyContent: paginatePage.pageNum ? "space-between" : "end", position: "static", bottom: "20px" }}>
-                        {paginatePage.pageNum ? (<ArrowCircleLeftOutlinedIcon cursor="pointer" onClick={handlePageBackward} sx={{ fontSize: 40, "&:hover": { opacity: .7 } }} />) : ('')}
-                        {paginatePage.nextPage ? (<ArrowCircleRightOutlinedIcon cursor="pointer" onClick={handlePageForward} sx={{ fontSize: 40, "&:hover": { opacity: .7 } }} />) : ('')}
+        <>
+            <Grid item xs={11} sx={{ py: 3, display: "flex", flexDirection: "column", justifyContent: "space-between" }} >
+
+                {doctorsData.length ? (
+                    <>
+                        <Box sx={{ flexGrow: 1 }}>
+                            <TableContainer component={Paper} >
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <StyledTableCell>Name</StyledTableCell>
+                                            <StyledTableCell>E-mail</StyledTableCell>
+                                            <StyledTableCell>Phone No.</StyledTableCell>
+                                            <StyledTableCell>Education</StyledTableCell>
+                                            {/* <StyledTableCell>Verified</StyledTableCell> */}
+                                            <StyledTableCell>Details</StyledTableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {doctorsData.length && doctorsData.map((doctorRow, indx) => (
+                                            <StyledTableRow key={doctorRow._id}>
+                                                <StyledTableCell>{`${doctorRow.firstName} ${doctorRow.lastName}`}</StyledTableCell>
+                                                <StyledTableCell>{doctorRow.email}</StyledTableCell>
+                                                <StyledTableCell>{doctorRow.phoneNumber}</StyledTableCell>
+                                                {/* <StyledTableCell>{doctorRow.isVerified ? "Yes" : "No"}</StyledTableCell> */}
+                                                <StyledTableCell>{doctorRow.education[0].degree}</StyledTableCell>
+                                                <StyledTableCell><Button variant="contained" onClick={() => { handleDrawerOpen(doctorRow) }}>More details</Button></StyledTableCell>
+                                                {/* <StyledTableCell><Button variant="contained" onClick={(function (doctorRow) { let data = doctorRow; return (() => { let data = doctorRow; handleDrawerOpen(data) }) })(doctorRow)}>More details</Button></StyledTableCell> */}
+
+                                            </StyledTableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Box>
+                        <Box sx={{ my: 3, px: 15, display: "flex", justifyContent: paginatePage.pageNum ? "space-between" : "end", position: "static", bottom: "20px" }}>
+                            {paginatePage.pageNum ? (<ArrowCircleLeftOutlinedIcon cursor="pointer" onClick={handlePageBackward} sx={{ fontSize: 40, "&:hover": { opacity: .7 } }} />) : ('')}
+                            {paginatePage.nextPage ? (<ArrowCircleRightOutlinedIcon cursor="pointer" onClick={handlePageForward} sx={{ fontSize: 40, "&:hover": { opacity: .7 } }} />) : ('')}
+                        </Box>
+                        <Snackbar open={snakbarStatus} autoHideDuration={5000} onClose={() => setSnakbarStatus(false)}>
+                            <CustomAlert
+                                severity={alertStatus.severity}
+                                action={
+                                    <IconButton
+                                        aria-label="close"
+                                        color="inherit"
+                                        size="small"
+                                        onClick={() => {
+                                            setSnakbarStatus(false);
+                                        }}
+                                    >
+                                        <CloseIcon fontSize="inherit" />
+                                    </IconButton>
+                                }
+                            >
+                                {alertStatus.msg}
+                            </CustomAlert>
+                        </Snackbar>
+                    </>
+                ) : (
+                    <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <CircularProgress color='highlight' sx={{ marginY: '10px' }} />
                     </Box>
-                    <Snackbar open={snakbarStatus} autoHideDuration={5000} onClose={() => setSnakbarStatus(false)}>
-                        <CustomAlert
-                            severity={alertStatus.severity}
-                            action={
-                                <IconButton
-                                    aria-label="close"
-                                    color="inherit"
-                                    size="small"
-                                    onClick={() => {
-                                        setSnakbarStatus(false);
-                                    }}
-                                >
-                                    <CloseIcon fontSize="inherit" />
-                                </IconButton>
-                            }
-                        >
-                            {alertStatus.msg}
-                        </CustomAlert>
-                    </Snackbar>
-                </>
-            ) : (
-                <Box sx={{ display: "flex", justifyContent: "center" }}>
-                    <CircularProgress color='highlight' sx={{ marginY: '10px' }} />
-                </Box>
-            )
-            }
-        </Grid >
+                )
+                }
+                <ReviewDoctorApplication open={open} handleDrawerClose={handleDrawerClose} currentDoctorRow={currentDoctorRow} />
+            </Grid >
+        </>
+
     )
 }
---
+++ view/src/Pages/AdminDashboard/ReviewDoctorApplication.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import {
+    Drawer, Divider, Typography, Grid, Table, TableHead, TableRow, TableBody, Button, id, TableContainer, Paper, Snackbar, Box
+} from '@mui/material';
+import { styled, useTheme } from '@mui/material/styles';
+import { StyledTableCell, StyledTableRow } from "../../Helper/CustomTableItems";
+
+import IconButton from '@mui/material/IconButton';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import licenceImg from '../../../src/Assets/Images/okasha.jpg'
+
+export default function ReviewDoctorApplication({ open, handleDrawerClose, currentDoctorRow }) {
+    console.log('currentDoctorRow: ', currentDoctorRow)
+
+    const DrawerHeader = styled('div')(({ theme }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-start',
+    }));
+    const drawerWidth = `${100 - 2 * 100 / 12}%`;
+
+    const theme = useTheme();
+
+    return (
+        <Drawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                },
+                backgroundColor: "red"
+            }}
+            variant="persistent"
+            anchor="right"
+            open={open}
+        >
+            <DrawerHeader>
+                <IconButton onClick={handleDrawerClose}>
+                    <ChevronRightIcon />
+                </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <Grid container alignItems="center" justifyContent="center" sx={{ height: "100%" }}>
+                <Grid item xs={11} >
+                    <TableContainer component={Paper} >
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell>Name</StyledTableCell>
+                                    <StyledTableCell>E-mail</StyledTableCell>
+                                    <StyledTableCell>Phone No.</StyledTableCell>
+                                    <StyledTableCell>Verified</StyledTableCell>
+                                    <StyledTableCell>Education</StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+
+                                <StyledTableRow key={currentDoctorRow?._id}>
+                                    <StyledTableCell>{`${currentDoctorRow?.firstName} ${currentDoctorRow?.lastName}`}</StyledTableCell>
+                                    <StyledTableCell>{currentDoctorRow?.email}</StyledTableCell>
+                                    <StyledTableCell>{currentDoctorRow?.phoneNumber}</StyledTableCell>
+                                    <StyledTableCell>{currentDoctorRow?.isVerified ? "Yes" : "No"}</StyledTableCell>
+                                    {/* {currentDoctorRow && <StyledTableCell>{currentDoctorRow?.education[0].degree}</StyledTableCell>} */}
+                                </StyledTableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={11} container justifyContent="space-between">
+                    <Grid item md={6} >
+                        <Typography variant="h5" sx={{ mb: 2 }}>Professional License:</Typography>
+                        <img
+                            // src={currentDoctorRow.professionalLicense} 
+                            src={licenceImg}
+                            alt="professional license"
+                            style={{ borderRadius: "5%", maxWidth: "80%", border: "3px solid grey" }}
+                        />
+                    </Grid>
+                    <Grid item md={6} sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "space-between" }}>
+                        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "space-between" }}>
+                            <Typography variant="h5">Experience:</Typography>
+                            {currentDoctorRow?.experiences?.map((exp, index) => (<Box>
+                                {/* <span style={{ display: "inline" }}>&rarr;Exprience::</span> */}
+                                <ul style={{ listStyleType: "none", margin: 0, padding: 0, display: "inline", marginBottom: "10px", border: "3px solid #fff" }}>
+                                    <li>{`${exp.title} at ${exp.workplace}`}</li>
+                                    <li>{`From: ${exp.startDate.slice(0, 10)} to: ${exp.endDate.slice(0, 10)}`}</li>
+                                    <li>{`Location: ${exp.location.city}, ${exp.location.country}`}</li>
+                                </ul>
+                            </Box>))}
+                        </Box>
+                        <Box sx={{ p: 5, display: "flex", justifyContent: "start" }}>
+                            <Button variant="contained" color="success" sx={{ mr: 7, px: 3, py: 1 }}>Accept</Button>
+                            <Button variant="contained" color="error" sx={{ px: 3 }}>Reject</Button>
+                        </Box>
+                    </Grid>
+                </Grid>
+
+            </Grid>
+        </Drawer >
+    )
+}
