461b4c5f69a1cac84e24b5e5a9e3955f71a9086d
Basem98 <basem.mostafa98@hotmail.com>
2022-08-19 08:58:06 +0200

    refactor: Populate the appointments in the getDoctorById controller's result

--- controller/doctors/getDoctorById.controller.js
+++ controller/doctors/getDoctorById.controller.js
@@ -1,18 +1,49 @@
-const { mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const Doctor = require('../../model/doctor.model');
+const Appointment = require('../../model/appointment.model')
 
 const getDoctorById = async (req, res, next) => {
     let doctorId = req.params.id;
     if (!mongoose.isValidObjectId(doctorId)) {
         return res.status(404).json({ message: 'Doctor is Not Found' });
     }
-    await Doctor.findOne({ _id: doctorId })
-        .populate('appointments')
+    await Doctor
+        .aggregate([
+            // 1. Only get doctors who have verified their email and been reviewed & accepted
+            {
+                $match: { _id: mongoose.Types.ObjectId(doctorId) }
+            },
+            // 2. Don't send the professionalLicense image source along with the data
+            {
+                $project: { professionalLicense: 0, password: 0 },
+            },
+            // 7. Populate the appointments array
+            {
+                $lookup: {
+                    from: Appointment.collection.name,
+                    as: 'appointments',
+                    localField: 'appointments._id',
+                    foreignField: '_id'
+                }
+            },
+            // 8. Filter the appointments by their state (Use $addFields instead of $project to keep the other properties)
+            {
+                $addFields: {
+                    'appointments':
+                    {
+                        $filter: {
+                            input: '$appointments',
+                            as: 'appointment',
+                            cond: { $eq: ['$$appointment.state', 'available'] }
+                        }
+                    }
+                }
+            }
+        ])
         .then((data) => {
             if (data) {
                 // Execluding password from the retrieved data
-                data.password = undefined;
-                return res.status(200).json({ message: data })
+                return res.status(200).json({ message: data[0] })
             }
             return res.status(404).json({ message: 'Doctor is Not Found' });
         })
