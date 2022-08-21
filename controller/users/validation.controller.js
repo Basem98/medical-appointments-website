24e02c0875c67a0ff5e329b21e6453d365d2d9e4
unknown <belalelmeligy516@gmail.com>
2022-08-21 08:11:27 +0200

    feat: Add validation for updated password

--- controller/users/validation.controller.js
+++ controller/users/validation.controller.js
@@ -17,3 +17,17 @@ module.exports.validateUserData = () => {
         body('phoneNumber').isMobilePhone('ar-EG').withMessage('Invalid Phone Number')
     ];
 }
+
+module.exports.validateUpdatedPassword = () => {
+    return [
+        body('password')
+            .isStrongPassword({
+                minLength: 8,
+                minLowercase: 1,
+                minUppercase: 1,
+                minNumbers: 1,
+                minSymbols: 1
+            })
+            .withMessage('Password should be at least 8 characters long and it should consist of at least one uppercase letter, one lowercase letter, one number, and one symbol')
+    ];
+}
