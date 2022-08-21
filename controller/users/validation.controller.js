5a272924e2a4d06053f21b228b4a0f48689feb2d
unknown <belalelmeligy516@gmail.com>
2022-08-21 09:24:36 +0200

    feat: Add validation array for user settings

--- controller/users/validation.controller.js
+++ controller/users/validation.controller.js
@@ -31,3 +31,11 @@ module.exports.validateUpdatedPassword = () => {
             .withMessage('Password should be at least 8 characters long and it should consist of at least one uppercase letter, one lowercase letter, one number, and one symbol')
     ];
 }
+
+module.exports.validateUserSettings = () => {
+    return [
+        body('firstName').isLength({ min: 2, max: 50 }).withMessage('First Name length between 2 to 50 characters'),
+        body('lastName').isLength({ min: 2, max: 50 }).withMessage('Last Name length between 2 to 50 characters'),
+        body('phoneNumber').isMobilePhone('ar-EG').withMessage('Invalid Phone Number')
+    ];
+}
