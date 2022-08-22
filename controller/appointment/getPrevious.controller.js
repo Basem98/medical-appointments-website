eb70bf9770ebbcef65038905033eb7530c6c375f
unknown <belalelmeligy516@gmail.com>
2022-08-22 07:02:31 +0200

    fix: Retrieve the previous appointment based on the correct criteria (state finished or appointment date is past)

--- controller/appointment/getPrevious.controller.js
+++ controller/appointment/getPrevious.controller.js
@@ -10,30 +10,49 @@ const getPrevious = (req, res, next) => {
     if (req.body.id != req.params.id.toString()) {
         return res.status(401).send({ message: 'Not Authorized to see this appointment' });
     }
-    Appointment.find({
-        user: { "$exists": true },
-        "$or": [
-            { user: req.params.id },
-            { doctor: req.params.id }
-        ],
-        "$or": [
-            { date: { "$lt": currentDate } },
-            {state: 'finished'}
-
-        ],
-    })
-        .populate('user')
-        .populate('doctor')
-        .then((data) => {
-            if (!data) {
-                return res.status(404).send({ message: 'Appointment Not Found' });
-            }
-            return res.status(200).json({ message: data })
+    console.log(req.body.role);
+    if (req.body.role === 'User') {
+        Appointment.find({
+            user: req.params.id,
+            "$or": [
+                { date: { "$lt": currentDate } },
+                { state: 'finished' }
+            ]
         })
-        .catch(error => {
-            error.statusCode = 500
-            next(error);
+            .populate('user')
+            .populate('doctor')
+            .then((data) => {
+                if (!data) {
+                    return res.status(404).send({ message: 'Appointment Not Found' });
+                }
+                return res.status(200).json({ message: data })
+            })
+            .catch(error => {
+                error.statusCode = 500
+                next(error);
+            })
+    } else if (req.body.role === 'Doctor') {
+        Appointment.find({
+            user: { "$exists": true },
+            doctor: req.params.id,
+            "$or": [
+                { date: { "$lt": currentDate } },
+                { state: 'finished' }
+            ],
         })
+            .populate('user')
+            .populate('doctor')
+            .then((data) => {
+                if (!data) {
+                    return res.status(404).send({ message: 'Appointment Not Found' });
+                }
+                return res.status(200).json({ message: data })
+            })
+            .catch(error => {
+                error.statusCode = 500
+                next(error);
+            })
+    }
 }
 
 module.exports = { getPrevious }
